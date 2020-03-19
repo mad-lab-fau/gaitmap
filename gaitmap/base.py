1ee7718a5ed965661edc9b066b9dfdb72b6e944d
Arne Küderle <arne.kuederle@fau.de>
2020-03-19 13:04:26 +0100

    Typo

--- gaitmap/base.py
+++ gaitmap/base.py
@@ -33,22 +33,20 @@ class BaseAlgorithm:
             List of parameter names of the algorithm
 
         """
-        # fetch the constructor or the original constructor before
-        # deprecation wrapping if any
+        # fetch the constructor or the original constructor before deprecation wrapping if any
         init = cls.__init__
         if init is object.__init__:
             # No explicit constructor to introspect
             return []
 
-        # introspect the constructor arguments to find the model parameters
-        # to represent
+        # introspect the constructor arguments to find the model parameters to represent
         init_signature = inspect.signature(init)
         # Consider the constructor parameters excluding 'self'
         parameters = [p for p in init_signature.parameters.values() if p.name != "self" and p.kind != p.VAR_KEYWORD]
         for p in parameters:
             if p.kind == p.VAR_POSITIONAL:
                 raise RuntimeError(
-                    "gaitmap-algorthms should always specify their parameters in the signature of their "
+                    "gaitmap-algorithms should always specify their parameters in the signature of their "
                     "__init__ (no  varargs). {} with constructor {} doesn't  follow this convention.".format(
                         cls, init_signature
                     )
