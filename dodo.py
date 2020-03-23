fa07ef8a8aeab05bad6890459a5c32aefbbca891
Arne Küderle <arne.kuederle@fau.de>
2020-03-23 22:58:57 +0100

    Provided proper documentation for doit in the README

--- dodo.py
+++ dodo.py
@@ -1,24 +1,31 @@
 from pathlib import Path
 import platform
 
-DOIT_CONFIG = {"default_tasks": ["format", "pytest", "lint"]}
+DOIT_CONFIG = {
+    "default_tasks": ["format", "pytest", "lint"],
+    "backend": "json",
+}
 
 HERE = Path(__file__).parent
 
 
 def task_format():
+    """Reformat all files using lint."""
     return {"actions": [["black", HERE]], "verbosity": 1}
 
 
-def task_pytest():
+def task_test():
+    """Run Pytest with coverage."""
     return {"actions": [["pytest", "--cov=gaitmap"]], "verbosity": 2}
 
 
 def task_lint():
+    """Lint all files with Prospector."""
     return {"actions": [["prospector"]], "verbosity": 1}
 
 
 def task_docs():
+    """Build the html docs using Sphinx."""
     if platform.system() == "Windows":
         return {"actions": [[HERE / "docs/make.bat", "html"]], "verbosity": 2}
     else:
