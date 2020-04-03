ac6939d2b7839c3b944b10b8433a90561398697e
Martin Ullrich <martin.ullrich@fau.de>
2020-04-03 13:47:06 +0200

    basic interfaces between methods set up

--- gaitmap/event_detection/rampp_event_detection.py
+++ gaitmap/event_detection/rampp_event_detection.py
@@ -12,6 +12,7 @@ from slither.imu.utils.static_moment_detection import sliding_window_view
 
 
 from gaitmap.base import BaseEventDetection, BaseType
+from gaitmap.utils.consts import BF_COLS, BF_ACC, BF_GYR
 
 
 class RamppEventDetection(BaseEventDetection):
@@ -33,7 +34,6 @@ class RamppEventDetection(BaseEventDetection):
 
     Other Parameters
     ----------------
-    TODO add other paramters
 
     Notes
     -----
@@ -56,11 +56,7 @@ class RamppEventDetection(BaseEventDetection):
     sampling_rate_hz: float
     segmented_stride_list: pd.DataFrame
 
-    def __init__(
-        self,
-        ic_search_region: Tuple[float, float] = None,
-        min_vel_search_wind_size: float = None
-    ):
+    def __init__(self, ic_search_region: Tuple[float, float] = (80, 50), min_vel_search_wind_size: float = 100):
         self.ic_search_region = ic_search_region
         self.min_vel_search_wind_size = min_vel_search_wind_size
 
@@ -91,73 +87,82 @@ class RamppEventDetection(BaseEventDetection):
 
         ic_search_region = tuple(int(v / 1000 * self.sampling_rate_hz) for v in self.ic_search_region)
         min_vel_search_wind_size = int(self.min_vel_search_wind_size / 1000 * self.sampling_rate)
+
+        acc = data[BF_ACC]
+        gyr = data[BF_GYR]
+
         self.ic_, self.tc_, self.min_vel_ = self._find_all_events(
-            gyro, acc, self.segmented_stride_list, ic_search_region, min_vel_search_wind_size
+            gyr, acc, self.segmented_stride_list, ic_search_region, min_vel_search_wind_size
         )
 
         return self
 
     @staticmethod
-    def _find_all_events(gyro, acc, stride_list, hs_search_region, ms_search_wind_size):
-        gyr_saggital = gyro[:, 1]
-        acc_ant_post = acc[:, 0]
-        hs_events = []
-        to_events = []
-        ms_events = []
-        for i in range(len(stride_list)):
-            start = stride_list[i][0]
-            end = stride_list[i][1]
-            gyr_sec = gyro[start:end]
-            gyr_saggital_sec = gyr_saggital[start:end]
-            acc_sec = acc_ant_post[start:end]
+    def _find_all_events(
+        self,
+        gyr: pd.DataFrame,
+        acc: pd.DataFrame,
+        stride_list: pd.DataFrame,
+        ic_search_region: Tuple[float, float],
+        min_vel_search_wind_size: float,
+    ):
+        gyr = gyr.to_numpy()
+        gyr_ml = gyr["gyr_ml"].to_numpy()
+        acc_pa = acc["acc_pa"].to_numpy()
+        ic_events = []
+        fc_events = []
+        min_vel_events = []
+        for index, stride in stride_list.iterrows():
+            start = stride["start"]
+            end = stride["stop"]
+            gyr_sec = gyr[start:end]
+            gyr_ml_sec = gyr_ml[start:end]
+            acc_sec = acc_pa[start:end]
             gyr_grad = np.gradient(gyr_sec)
-            hs_events.append(start + _find_heel_strike(gyr_saggital_sec, acc_sec, gyr_grad, hs_search_region))
-            to_events.append(start + _find_toe_off(gyr_sec))
-            ms_events.append(start + _find_mid_stance(gyr_sec, ms_search_wind_size))
+            ic_events.append(start + self._detect_ic(gyr_ml_sec, acc_sec, gyr_grad, ic_search_region))
+            fc_events.append(start + self._detect_tc(gyr_sec))
+            min_vel_events.append(start + self._detect_min_vel(gyr_sec, min_vel_search_wind_size))
 
-        return np.array(hs_events, dtype=float), np.array(to_events, dtype=float), np.array(ms_events, dtype=float)
+        return np.array(ic_events, dtype=float), np.array(fc_events, dtype=float), np.array(min_vel_events, dtype=float)
 
     def _detect_ic(
-        gyr_sagittal: np.ndarray,
-        acc_ant_post: np.ndarray,
-        gyr_sagittal_grad: np.ndarray,
-        search_window: Tuple[float, float],
+        self, gyr_ml: np.ndarray, acc_pa: np.ndarray, gyr_ml_grad: np.ndarray, search_window: Tuple[float, float],
     ) -> int:
 
         # Determine rough search region
-        search_region = (np.argmax(gyr_sagittal), int(0.6 * len(gyr_sagittal)))
+        search_region = (np.argmax(gyr_ml), int(0.6 * len(gyr_ml)))
 
         if search_region[1] - search_region[0] <= 0:
-            # The gyro argmax was not found in the first half of the step
+            # The gyr argmax was not found in the first half of the step
             return np.nan
 
         # TODO: Redefine search region (does not work sometimes
         # alternative:
         # refined_search_region_start, refined_search_region_stop = search_region
-        refined_search_region_start = search_region[0] + np.argmin(gyr_sagittal_grad[slice(*search_region)])
+        refined_search_region_start = search_region[0] + np.argmin(gyr_ml_grad[slice(*search_region)])
         refined_search_region_stop = refined_search_region_start + np.argmax(
-            gyr_sagittal_grad[refined_search_region_start : search_region[1]]
+            gyr_ml_grad[refined_search_region_start : search_region[1]]
         )
 
         if refined_search_region_stop - refined_search_region_start <= 0:
             return np.nan
 
-        # Find heel strike candidate in search region based on gyro
+        # Find heel strike candidate in search region based on gyr
         heel_strike_candidate = refined_search_region_start + np.argmin(
-            gyr_sagittal[refined_search_region_start:refined_search_region_stop]
+            gyr_ml[refined_search_region_start:refined_search_region_stop]
         )
 
         # Acc search window
         acc_search_region_start = int(np.max(np.array([0, heel_strike_candidate - search_window[0]])))
-        acc_search_region_stop = int(np.min(np.array([len(acc_ant_post), heel_strike_candidate + search_window[1]])))
+        acc_search_region_stop = int(np.min(np.array([len(acc_pa), heel_strike_candidate + search_window[1]])))
 
-        return acc_search_region_start + np.argmin(acc_ant_post[acc_search_region_start:acc_search_region_stop])
+        return acc_search_region_start + np.argmin(acc_pa[acc_search_region_start:acc_search_region_stop])
 
-    def _detect_tc(gyr_sagittal: np.ndarray) -> int:
-        return np.where(np.diff(np.signbit(gyr_sagittal)))[0][0]
+    def _detect_tc(self, gyr_ml: np.ndarray) -> int:
+        return np.where(np.diff(np.signbit(gyr_ml)))[0][0]
 
-    def _detect_min_vel(gyro: np.ndarray, window_size: int) -> int:
-        energy = norm(gyro, axis=-1) ** 2
+    def _detect_min_vel(self, gyr: np.ndarray, window_size: int) -> int:
+        energy = norm(gyr, axis=-1) ** 2
         energy = sliding_window_view(energy, shape=(window_size,))
         min_vel_start = np.nanmin(energy)
         min_vel_center = min_vel_start + window_size // 2
