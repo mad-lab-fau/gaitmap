2a1f39111a5d07ad47e68805ae19611d7288e83d
Arne Küderle <arne.kuederle@fau.de>
2020-04-03 18:08:24 +0200

    Added new "use_cols" option

--- gaitmap/stride_segmentation/dtw_templates/templates.py
+++ gaitmap/stride_segmentation/dtw_templates/templates.py
@@ -1,6 +1,6 @@
 from importlib.resources import open_text
 from pathlib import Path
-from typing import Optional, Union
+from typing import Optional, Union, List
 import numpy as np
 import pandas as pd
 
@@ -10,6 +10,7 @@ from gaitmap.stride_segmentation import dtw_templates
 class DtwTemplate:
     sampling_rate_hz: Optional[float]
     template_file_name: Optional[str]
+    use_cols: Optional[List[Union[str, int]]]
 
     _template: Optional[Union[np.ndarray, pd.DataFrame]]
 
@@ -20,18 +21,32 @@ class DtwTemplate:
         If not dataset is registered yet, it will be read from the file path if provided.
         """
         try:
-            return self._template
+            template = self._template
         except AttributeError:
             if not self.template_file_name:
                 raise AttributeError("Neither a template array nor a template file is provided.")
             with open_text(dtw_templates, self.template_file_name) as test_data:
                 self._template = pd.read_csv(test_data, header=0)
-            return self._template
+            template = self._template
+
+        if getattr(self, "use_cols", None) is None:
+            return template
+
+        if isinstance(template, np.ndarray):
+            if template.ndim < 2:
+                raise ValueError("The stored template is only 1D, but a 2D array is required to use `use_cols`")
+            return np.squeeze(template[:, self.use_cols])
+        return template[self.use_cols]
 
 
-def create_dtw_template(template: Union[np.ndarray, pd.DataFrame], sampling_rate_hz: Optional[float]) -> DtwTemplate:
+def create_dtw_template(
+    template: Union[np.ndarray, pd.DataFrame],
+    sampling_rate_hz: Optional[float],
+    use_cols: Optional[List[Union[str, int]]] = None,
+) -> DtwTemplate:
     template_instance = DtwTemplate()
     template_instance._template = template
     template_instance.sampling_rate_hz = sampling_rate_hz
+    template_instance.use_cols = use_cols
 
     return template_instance
--- tests/test_stride_segmentation/test_dtw_templates.py
+++ tests/test_stride_segmentation/test_dtw_templates.py
@@ -1,8 +1,9 @@
+import numpy as np
+import pandas as pd
 import pytest
 from numpy.testing import assert_array_equal
 
 from gaitmap.stride_segmentation.dtw_templates import DtwTemplate, create_dtw_template
-import numpy as np
 
 
 class TestTemplateBaseClass:
@@ -22,6 +23,35 @@ class TestTemplateBaseClass:
         with pytest.raises(AttributeError):
             _ = instance.template
 
+    def test_use_columns_array(self):
+        template = np.stack((np.arange(10), np.arange(10, 20))).T
+
+        instance = DtwTemplate()
+        instance._template = template
+        instance.use_cols = [1]
+
+        assert_array_equal(instance.template, template[:, 1])
+
+    def test_use_columns_dataframe(self):
+        template = np.stack((np.arange(10), np.arange(10, 20))).T
+        template = pd.DataFrame(template, columns=["col_1", "col_2"])
+
+        instance = DtwTemplate()
+        instance._template = template
+        instance.use_cols = ["col_1"]
+
+        assert_array_equal(instance.template, template[["col_1"]])
+
+    def test_use_columns_wrong_dim(self):
+        template = np.arange(10)
+
+        instance = DtwTemplate()
+        instance._template = template
+        instance.use_cols = [1]
+
+        with pytest.raises(ValueError):
+            _ = instance.template
+
 
 class TestCreateTemplate:
     def test_create_template_simple(self):
@@ -32,3 +62,15 @@ class TestCreateTemplate:
 
         assert_array_equal(instance.template, template)
         assert instance.sampling_rate_hz == sampling_rate_hz
+
+    def test_create_template_use_col(self):
+        template = np.stack((np.arange(10), np.arange(10, 20))).T
+        template = pd.DataFrame(template, columns=["col_1", "col_2"])
+        sampling_rate_hz = 100
+        use_cols = ["col_1"]
+
+        instance = create_dtw_template(template, sampling_rate_hz=sampling_rate_hz, use_cols=use_cols)
+
+        assert_array_equal(instance.template, template[["col_1"]])
+        assert instance.sampling_rate_hz == sampling_rate_hz
+        assert_array_equal(instance.use_cols, use_cols)
