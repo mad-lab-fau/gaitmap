2c2435d915b93e2fa28eaff74f7b6dfe58348256
Nils Roth <nils.roth@fau.de>
2020-04-03 17:30:14 +0200

    moved sliding window function to new module array_handling, manually merged prospector

--- gaitmap/utils/array_handling.py
+++ gaitmap/utils/array_handling.py
@@ -1,64 +1,64 @@
-"""A set of util functions that help to find static windows in imu signals."""
+"""A set of util functions that help to manipulate arrays in any imaginable way."""
 import numpy as np
 
 
-def sliding_window_view(data: np.ndarray, window_length: int, window_overlap: int) -> np.ndarray:
+def sliding_window_view(arr: np.ndarray, window_length: int, overlap: int) -> np.ndarray:
     """Create a sliding window view of an input array with given window length and overlap.
 
     Parameters
     ----------
-    data : np.ndarray
+    arr : np.ndarray
         input data on which sliding window action should be performed, array with shape (1,) or (n, m) while windowing
         will be performed along axis 0
 
     window_length : int
         length of a desired window in samples
 
-    window_overlap : int
+    overlap : int
         length of desired overlap in samples
 
     Returns
     -------
-    np.ndarray : windowed view or windowed copy of input array
+    np.ndarray : windowed view input array
 
     Examples
     --------
     Sliding window of 4 samples and 50% overlap 1D array
     >>> data = np.arange(0,10)
-    >>> windowed_view = sliding_window_view(data = data, window_length = 4, window_overlap = 2)
+    >>> windowed_view = sliding_window_view(arr = data, window_length = 4, overlap = 2)
 
     Sliding window of 7 samples and 2 sample overlap 3D array
     >>> data = np.column_stack([np.arange(0, 10), np.arange(0, 10), np.arange(0, 10)])
-    >>> windowed_view = sliding_window_view(data = data, window_length = 7, window_overlap = 2)
+    >>> windowed_view = sliding_window_view(arr = data, window_length = 7, overlap = 2)
 
     """
-    if window_overlap >= window_length:
+    if overlap >= window_length:
         raise ValueError("Invalid Input, overlap must be smaller than window length!")
 
     if window_length < 2:
         raise ValueError("Invalid Input, window_length must be larger than 1!")
 
     # calculate length of necessary np.nan-padding to make sure windows and overlaps exactly fits data length
-    n_windows = np.ceil((len(data) - window_length) / (window_length - window_overlap)).astype(int)
-    pad_length = window_length + n_windows * (window_length - window_overlap) - len(data)
+    n_windows = np.ceil((len(arr) - window_length) / (window_length - overlap)).astype(int)
+    pad_length = window_length + n_windows * (window_length - overlap) - len(arr)
 
     # had to handle 1D arrays separately
-    if data.ndim == 1:
+    if arr.ndim == 1:
         pad = np.repeat(np.nan, pad_length)
-        data = np.append(data, pad)
+        arr = np.append(arr, pad)
 
-        new_shape = (data.size - window_length + 1, window_length)
+        new_shape = (arr.size - window_length + 1, window_length)
     else:
-        pad = np.ones((pad_length, np.shape(data)[-1])) * np.nan
-        data = np.append(data, pad, axis=0)
+        pad = np.ones((pad_length, np.shape(arr)[-1])) * np.nan
+        arr = np.append(arr, pad, axis=0)
 
-        shape = (window_length, data.shape[-1])
-        n = np.array(data.shape)
+        shape = (window_length, arr.shape[-1])
+        n = np.array(arr.shape)
         o = n - shape + 1  # output shape
         new_shape = np.concatenate((o, shape), axis=0)
 
     # apply stride_tricks magic
-    new_strides = np.concatenate((data.strides, data.strides), axis=0)
-    view = np.lib.stride_tricks.as_strided(data, new_shape, new_strides)[0:: (window_length - window_overlap)]
+    new_strides = np.concatenate((arr.strides, arr.strides), axis=0)
+    view = np.lib.stride_tricks.as_strided(arr, new_shape, new_strides)[0:: (window_length - overlap)]
 
     return np.squeeze(view)  # get rid of single-dimensional entries from the shape of an array.
--- tests/test_utils/test_array_handling.py
+++ tests/test_utils/test_array_handling.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 from numpy.testing import assert_array_equal
 
-from gaitmap.utils.static_moment_detection import sliding_window_view
+from gaitmap.utils.array_handling import sliding_window_view
 
 
 class TestSlidingWindow:
@@ -10,51 +10,51 @@ class TestSlidingWindow:
 
     def test_invalid_inputs_overlap(self):
         with pytest.raises(ValueError, match=r".* overlap .*"):
-            sliding_window_view(np.arange(0, 10), window_length=4, window_overlap=4)
+            sliding_window_view(np.arange(0, 10), window_length=4, overlap=4)
 
     def test_invalid_inputs_window_size(self):
         with pytest.raises(ValueError, match=r".* window_length .*"):
-            sliding_window_view(np.arange(0, 10), window_length=1, window_overlap=0)
+            sliding_window_view(np.arange(0, 10), window_length=1, overlap=0)
 
     def test_sliding_window_1D_without_edge_case(self):
         input = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
         expected_output = np.array([[0, 1, 2, 3], [2, 3, 4, 5], [4, 5, 6, 7], [6, 7, 8, 9]])
-        window_view = sliding_window_view(input, window_length=4, window_overlap=2)
+        window_view = sliding_window_view(input, window_length=4, overlap=2)
 
         assert_array_equal(expected_output, window_view)
 
     def test_sliding_window_1D_with_edge_case(self):
         input = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
         expected_output = np.array([[0, 1, 2, 3], [2, 3, 4, 5], [4, 5, 6, 7], [6, 7, 8, 9], [8, 9, 10, np.nan]])
-        window_view = sliding_window_view(input, window_length=4, window_overlap=2)
+        window_view = sliding_window_view(input, window_length=4, overlap=2)
 
         assert_array_equal(expected_output, window_view)
 
     def test_sliding_window_1D_with_edge_case2(self):
         input = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
         expected_output = np.array([[0, 1, 2, 3, 4, 5, 6], [5, 6, 7, 8, 9, np.nan, np.nan]])
-        window_view = sliding_window_view(input, window_length=7, window_overlap=2)
+        window_view = sliding_window_view(input, window_length=7, overlap=2)
 
         assert_array_equal(expected_output, window_view)
 
     def test_sliding_window_1D_no_overlap_without_edge_case(self):
         input = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8])
         expected_output = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
-        window_view = sliding_window_view(input, window_length=3, window_overlap=0)
+        window_view = sliding_window_view(input, window_length=3, overlap=0)
 
         assert_array_equal(expected_output, window_view)
 
     def test_sliding_window_1D_no_overlap_with_edge_case(self):
         input = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
         expected_output = np.array([[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, np.nan, np.nan]])
-        window_view = sliding_window_view(input, window_length=4, window_overlap=0)
+        window_view = sliding_window_view(input, window_length=4, overlap=0)
 
         assert_array_equal(expected_output, window_view)
 
     def test_sliding_window_1D_asym_overlap_with_edge_case(self):
         input = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
         expected_output = np.array([[0, 1, 2, 3, 4], [2, 3, 4, 5, 6], [4, 5, 6, 7, 8], [6, 7, 8, 9, np.nan]])
-        window_view = sliding_window_view(input, window_length=5, window_overlap=3)
+        window_view = sliding_window_view(input, window_length=5, overlap=3)
 
         assert_array_equal(expected_output, window_view)
 
