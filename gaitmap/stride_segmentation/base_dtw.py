1677c30b34a82155bfebf69c8f43d0715db6f4a6
Arne Küderle <arne.kuederle@fau.de>
2020-04-04 13:48:32 +0200

    Base DTW now uses a template instance instead template arrays

--- gaitmap/stride_segmentation/base_dtw.py
+++ gaitmap/stride_segmentation/base_dtw.py
@@ -7,6 +7,7 @@ from tslearn.utils import to_time_series
 from typing_extensions import Literal
 
 from gaitmap.base import BaseStrideSegmentation, BaseType
+from gaitmap.stride_segmentation.dtw_templates import DtwTemplate
 from gaitmap.stride_segmentation.utils import find_local_minima_with_distance, find_local_minima_below_threshold
 
 
@@ -79,15 +80,15 @@ class BaseDtw(BaseStrideSegmentation):
 
     Parameters
     ----------
-    template : (n x m) array representing a single stride
-        The template of length n used for matching. If the template has multiple dimensions m, the first m dimensions of
-        the data are used to perform the matching.
-    template_sampling_rate_hz
-        Sampling rate used for the template. This information is used to resample the template to the sampling rate of
-        the data if `resample_template` is `True`. If `resample_template` is `False` this information is ignored.
+    template
+        The template used for matching.
+        See more details in the :class:`DtwTemplates <gaitmap.stride_segmentation.dtw_templates.DtwTemplates>` docu.
+        If the template has its `sampling_rate_hz` attribute set, this information is used to resample the template to
+        the sampling rate of the data if `resample_template` is `True`. If `resample_template` is `False` this
+        information is ignored.
     resample_template
         If `True` the template will be resampled to match the sampling rate of the data.
-        This requires a valid value for `template_sampling_rate_hz`.
+        This requires a valid value for `template.sampling_rate_hz`.
     max_cost
         The maximal allowed cost to find potential stride candidates in the cost function.
         Its usage depends on the exact `find_matches_method` used.
@@ -114,8 +115,7 @@ class BaseDtw(BaseStrideSegmentation):
         The sampling rate of the data
     """
 
-    template: Optional[np.ndarray]
-    template_sampling_rate_hz: Optional[float]
+    template: Optional[DtwTemplate]
     max_cost: Optional[float]
     resample_template: bool
     min_stride_time_s: float
@@ -142,15 +142,13 @@ class BaseDtw(BaseStrideSegmentation):
 
     def __init__(
         self,
-        template: Optional[np.ndarray] = None,
-        template_sampling_rate_hz: Optional[float] = None,
+        template: Optional[DtwTemplate] = None,
         resample_template: bool = True,
         find_matches_method: Literal["original", "find_peaks"] = "original",
         max_cost: Optional[float] = None,
         min_stride_time_s: Optional[float] = 0.6,
     ):
         self.template = template
-        self.template_sampling_rate_hz = template_sampling_rate_hz
         self.max_cost = max_cost
         self.min_stride_time_s = min_stride_time_s
         self.resample_template = resample_template
@@ -185,15 +183,16 @@ class BaseDtw(BaseStrideSegmentation):
         if self.template is None:
             raise ValueError("A `template` must be specified.")
 
-        if self.resample_template and not self.template_sampling_rate_hz:
+        if self.resample_template and not self.template.sampling_rate_hz:
             raise ValueError(
-                "To resample the template (`resample_template=True`), `template_sampling_rate_hz` must be specified."
+                "To resample the template (`resample_template=True`), a `sampling_rate_hz` must be specified for the "
+                "template."
             )
 
-        if self.resample_template is True and sampling_rate_hz != self.template_sampling_rate_hz:
+        if self.resample_template is True and sampling_rate_hz != self.template.sampling_rate_hz:
             template = self._interpolate_template(sampling_rate_hz)
         else:
-            template = self.template
+            template = self.template.template
 
         min_distance = None
         if self.min_stride_time_s not in (None, 0, 0.0):
@@ -219,7 +218,8 @@ class BaseDtw(BaseStrideSegmentation):
 
     def _interpolate_template(self, new_sampling_rate: float) -> np.ndarray:
         template = resample(
-            self.template, int(self.template.shape[0] * new_sampling_rate / self.template_sampling_rate_hz)
+            self.template.template,
+            int(self.template.template.shape[0] * new_sampling_rate / self.template_sampling_rate_hz),
         )
         return template
 
--- tests/test_stride_segmentation/test_base_dtw.py
+++ tests/test_stride_segmentation/test_base_dtw.py
@@ -3,6 +3,7 @@ import pytest
 
 from gaitmap.base import BaseType
 from gaitmap.stride_segmentation.base_dtw import BaseDtw
+from gaitmap.stride_segmentation.dtw_templates import create_dtw_template
 from tests.mixins.test_algorithm_mixin import TestAlgorithmMixin
 
 
@@ -12,26 +13,19 @@ class TestMetaFunctionality(TestAlgorithmMixin):
 
     @pytest.fixture()
     def after_action_instance(self) -> BaseType:
-        dtw = BaseDtw(
-            template=np.array([0, 1.0, 0]), template_sampling_rate_hz=100.0, max_cost=0.5, min_stride_time_s=None,
-        )
+        template = create_dtw_template(np.array([0, 1.0, 0]), sampling_rate_hz=100.0)
+        dtw = BaseDtw(template=template, max_cost=0.5, min_stride_time_s=None,)
         data = np.array([0, 1.0, 0])
         dtw.segment(data, sampling_rate_hz=100)
         return dtw
 
 
 class TestSimpleSegment:
-    template = np.array([0, 1.0, 0])
+    template = create_dtw_template(np.array([0, 1.0, 0]), sampling_rate_hz=100.0)
 
     @pytest.fixture(params=list(BaseDtw._allowed_methods_map.keys()), autouse=True)
     def _create_instance(self, request):
-        dtw = BaseDtw(
-            template=self.template,
-            template_sampling_rate_hz=100.0,
-            max_cost=0.5,
-            min_stride_time_s=None,
-            find_matches_method=request.param,
-        )
+        dtw = BaseDtw(template=self.template, max_cost=0.5, min_stride_time_s=None, find_matches_method=request.param,)
         self.dtw = dtw
 
     def test_sdtw_simple_multi_match(self):
@@ -41,7 +35,7 @@ class TestSimpleSegment:
 
         np.testing.assert_array_equal(dtw.paths_, [[(0, 5), (1, 6), (2, 7)]])
         assert dtw.costs_ == [0.0]
-        np.testing.assert_array_equal(dtw.strides_start_end_, [[5, 7]])
+        np.testing.assert_array_equal(dtw.matches_start_end_, [[5, 7]])
         np.testing.assert_array_equal(
             dtw.acc_cost_mat_,
             [
@@ -52,7 +46,6 @@ class TestSimpleSegment:
         )
 
         np.testing.assert_array_equal(dtw.data, sequence)
-        np.testing.assert_array_equal(dtw.template, self.template)
 
     def test_sdtw_multi_match(self):
         sequence = 2 * [*np.ones(5) * 2, 0, 1.0, 0, *np.ones(5) * 2]
@@ -60,11 +53,10 @@ class TestSimpleSegment:
         dtw = self.dtw.segment(np.array(sequence), sampling_rate_hz=100.0,)
 
         np.testing.assert_array_equal(dtw.paths_, [[(0, 5), (1, 6), (2, 7)], [(0, 18), (1, 19), (2, 20)]])
-        np.testing.assert_array_equal(dtw.strides_start_end_, [[5, 7], [18, 20]])
+        np.testing.assert_array_equal(dtw.matches_start_end_, [[5, 7], [18, 20]])
         np.testing.assert_array_equal(dtw.costs_, [0.0, 0.0])
 
         np.testing.assert_array_equal(dtw.data, sequence)
-        np.testing.assert_array_equal(dtw.template, self.template)
 
 
 # TODO; Test template interpolate
