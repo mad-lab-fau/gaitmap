8a427998ed358b04a4d562e5d5ebc37aff16704d
Alzhraa Ibrahim <259-ri75luwi@users.noreply.mad-srv.informatik.uni-erlangen.de>
2020-04-06 16:53:46 +0200

    change static method to class method

--- gaitmap/temporal_parameters/temporal_parameters.py
+++ gaitmap/temporal_parameters/temporal_parameters.py
@@ -32,17 +32,11 @@ class TemporalParameterCalculation(BaseTemporalParameterCalculation):
     """
 
     parameters_: pd.DataFrame  # Results are stored here
-    stride_id_ = []
-    time_stamp_ = []
-    stride_time_ = []
-    swing_time_ = []
-    stance_time_ = []
 
     def __init__(self):
         pass
 
-    @staticmethod
-    def calculate(gait_events: RamppEventDetection, sampling_rate_hz: float) -> BaseType:
+    def calculate(self: BaseType, gait_events: RamppEventDetection, sampling_rate_hz: float) -> BaseType:
         """Find temporal parameters in in strides after segmentation and detecting events of each stride.
 
         Parameters
@@ -58,33 +52,37 @@ class TemporalParameterCalculation(BaseTemporalParameterCalculation):
                 The class instance with temporal parameters populated in parameters_
 
         """
+        stride_id_ = []
+        time_stamp_ = []
+        stride_time_ = []
+        swing_time_ = []
+        stance_time_ = []
         param = dict.fromkeys(["left_sensor", "right_sensor"])
         keys = ["stride_id", "timestamp", "stride_time", "swing_time", "stance_time"]
         for sensor in gait_events.data:
             for index, stride in gait_events.data[sensor].iterrows():
-                self.stride_id_.append(stride["s_id"])
-                self.time_stamp_.append(self._calc_timestamp(stride["ic"], sampling_rate_hz))
-                self.stride_time_.append(self._calc_stride_time(stride["ic"], stride["pre_ic"], sampling_rate_hz))
-                self.swing_time_.append(self._calc_swing_time(stride["ic"], stride["tc"], sampling_rate_hz))
-                self.stance_time_.append(self.stride_time[index], self.swing_time[index])
-            dic = dict(
-                zip(keys, ([self.stride_id_, self.time_stamp_, self.stride_time_, self.swing_time_, self.stance_time_]))
-            )
+                stride_id_.append(stride["s_id"])
+                time_stamp_.append(_calc_time_stamp_(stride["ic"], sampling_rate_hz))
+                stride_time_.append(_calc_stride_time(stride["ic"], stride["pre_ic"], sampling_rate_hz))
+                swing_time_.append(_calc_swing_time(stride["ic"], stride["tc"], sampling_rate_hz))
+                stance_time_.append(stride_time_[index], swing_time_[index])
+            dic = dict(zip(keys, ([stride_id_, time_stamp_, stride_time_, swing_time_, stance_time_])))
             param[sensor] = dic
         self.parameters_ = pd.DataFrame.from_dict(param)
         return self
 
 
-def _calc_time_stamp(self, ic_event: float, sampling_rate_hz: float) -> float:
+def _calc_time_stamp_(ic_event: float, sampling_rate_hz: float) -> float:
     return ic_event / sampling_rate_hz
 
 
-def _calc_stride_time(self, ic_event: float, pre_ic_event: float, sampling_rate_hz: float) -> float:
+def _calc_stride_time(ic_event: float, pre_ic_event: float, sampling_rate_hz: float) -> float:
     return (ic_event - pre_ic_event) / sampling_rate_hz
 
 
-def _calc_swing_time(self, ic_event: float, tc_event: float, sampling_rate_hz: float) -> float:
+def _calc_swing_time(ic_event: float, tc_event: float, sampling_rate_hz: float) -> float:
     return (ic_event - tc_event) / sampling_rate_hz
 
-def _calc_stance_time(self, stride_time: float, swing_time: float) -> float:
+
+def _calc_stance_time(stride_time: float, swing_time: float) -> float:
     return stride_time - swing_time
