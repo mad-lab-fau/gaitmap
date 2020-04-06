fix order of rampp-quaternion to suit scipy rotations

gaitmap/trajectory_reconstruction/orientation_estimation.py
@@ -68,12 +68,12 @@ class GyroIntegration(BaseOrientationEstimation):
         print('TEST: ', len(sensor_data))
         for i_sample in range(1, len(sensor_data)+1):
             self.estimated_orientations_.append(
-                self._next_quaternion(
+                self._next_quaternion_rampp(
                     self.estimated_orientations_[i_sample - 1], sensor_data[SF_GYR].iloc[i_sample-1]
                 )
             )
 
-    def _next_quaternion(self, previous_quaternion, gyr: pd.Series) -> Rotation:
+    def _next_quaternion_zrenner(self, previous_quaternion, gyr: pd.Series) -> Rotation:
         # TODO: Check if gyroscope data is in degeree or rad
         x = 0
         y = 1
@@ -81,7 +81,18 @@ class GyroIntegration(BaseOrientationEstimation):
 
         diff_quaternion_gyro = np.multiply(1 / (2 * self.sampling_rate_hz), [0, gyr[x], gyr[y], gyr[z]])
         diff_quaternion_gyro = self._exp(diff_quaternion_gyro)
+        di
         return Rotation(previous_quaternion * diff_quaternion_gyro)
+
+    def _next_quaternion_rampp(self, previous_quaternion, gyr: pd.Series) -> Rotation:
+        x = 0
+        y = 1
+        z = 2
+        diff_quaternion_gyro = np.multiply(1 / (2 * self.sampling_rate_hz), [gyr[x], gyr[y], gyr[z], 0])
+        # format quaternion anpassen
+        new_quat = previous_quaternion.as_quat() + diff_quaternion_gyro
+        new_quat /= np.linalg.norm(new_quat, 2)
+        return Rotation(new_quat)
 
     #@staticmethod
     def _exp(self, q: np.ndarray) -> np.ndarray:

tests/test_orientation_estimation/test_orientation_estimation.py
@@ -10,20 +10,21 @@ class TestGyroIntegration:
 
     def test_180_x(self):
         sensor_data = pd.DataFrame(columns=SF_GYR)
-        fs = 10
+        fs = 100
         # 180 degree rotation around first axis
         sensor_data[SF_GYR[0]] = [0] * fs
-        sensor_data[SF_GYR[1]] = [np.pi/fs] * fs
+        sensor_data[SF_GYR[1]] = [np.pi] * fs
         sensor_data[SF_GYR[2]] = [0] * fs
         sensor_data[SF_ACC[0]] = [0] * fs
         sensor_data[SF_ACC[1]] = [1] * fs
         sensor_data[SF_ACC[2]] = [0] * fs
 
-        gyr_integrator = GyroIntegration(Rotation([0, 0, 0, 1]))
+        gyr_integrator = GyroIntegration(Rotation([0, 1, 0, 0]))
         gyr_integrator.estimate_orientation_sequence(sensor_data, fs)
-        Rotation.from_euler('x', 180, degrees=True).as_quat()
         print('\n')
         print('Result:')
-        print(gyr_integrator.estimated_orientations_[-1].as_quat())
+        orientations = gyr_integrator.estimated_orientations_
+        for i_sample in orientations:
+            print(i_sample.as_quat())
 
         #np.testing.assert_array_almost_equal(gyr_integrator.estimated_orientations_[-1], )