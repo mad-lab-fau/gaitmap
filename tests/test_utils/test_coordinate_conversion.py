9ca498dae1e44681d3eec1f876e967c07cb2f65f
Felix Kluge <felix.kluge@fau.de>
2020-04-06 12:14:40 +0200

    Added test of single sensor data frames

---
+++ tests/test_utils/test_coordinate_conversion.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import pytest
+from pandas.testing import assert_frame_equal
+from gaitmap.utils.consts import SF_COLS, BF_COLS
+
+from gaitmap.utils.coordinate_conversion import (
+    convert_left_foot,
+    convert_right_foot
+)
+
+
+class TestSingleSensors:
+    """Test the functions for converting either left or right foot"""
+
+    @pytest.fixture(autouse=True)
+    def _sample_sensor_data(self):
+        """Create some sample dummy data frames"""
+        self.data_left = pd.DataFrame([[1, 2, 3, 4, 5, 6]], columns=SF_COLS)
+        self.data_right = pd.DataFrame([[7, 8, 9, 10, 11, 12]], columns=SF_COLS)
+
+        # Create expected output
+        self.data_left_expected = pd.DataFrame([[1, 2, -3, -4, -5, -6]], columns=BF_COLS)
+        self.data_right_expected = pd.DataFrame([[7, -8, -9, 10, -11, 12]], columns = BF_COLS)
+
+    def test_convert_left_foot(self):
+        data_converted = convert_left_foot(self.data_left)
+        assert_frame_equal(data_converted, self.data_left_expected)
+
+    def test_convert_right_foot(self):
+        data_converted = convert_right_foot(self.data_right)
+        assert_frame_equal(data_converted, self.data_right_expected)
