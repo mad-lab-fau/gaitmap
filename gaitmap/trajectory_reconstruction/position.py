6b12663f2e2bd7aa725836d084a851d4544b1edc
Malte Ollenschlaeger <malte.ollenschlaeger@fau.de>
2020-04-07 14:29:06 +0200

    add tests

--- gaitmap/trajectory_reconstruction/position.py
+++ gaitmap/trajectory_reconstruction/position.py
@@ -5,6 +5,7 @@ import pandas as pd
 from scipy import integrate
 
 from gaitmap.base import BasePositionEstimation
+from gaitmap.utils import dataset_helper
 from gaitmap.utils.consts import SF_ACC
 
 
@@ -16,34 +17,47 @@ class ForwardBackwardIntegration(BasePositionEstimation):
     axis (=z-axis or superior-inferior-axis, see :ref:`ff`.
     """
 
-    # TODO: add support for multiple sensors
+    # TODO: add support for multiple sensors and adapt tests accordingly
     sampling_rate_hz: float
     # TODO: add support for body frame axes (medical axes), e.g. using
     #  velocity.columns.map({'acc_ml':'vel_ml',...})
 
-    # TODO: the next two lines don't need to be in "self", right? Put them some
+    # TODO: the next two lines don't need to be in "self", right? Put them somewhere else? consts?
 
     vel_axis_names = [i_axis.replace("acc", "vel") for i_axis in SF_ACC]
     pos_axis_names = [i_axis.replace("acc", "vel") for i_axis in SF_ACC]
 
-    velocity: pd.DataFrame
-    position: pd.DataFrame
+    velocity_: pd.DataFrame
+    position_: pd.DataFrame
+    steepness: float
+    turning_point: float
 
     # TODO: this was taken from Julius' Benchmarking paper, adapt to default value but make it possible to pass a
     #  different x0 to estimate
     x0 = 0.6  # TODO: make this optional
 
-    def __init__(self, x0):
-        self.x0 = x0
+    def __init__(self, turning_point, steepness):
+        # TODO: specify what x0 means
+        self.turning_point = turning_point
+        self.steepness = steepness
 
-    def estimate(self, data):
+    def estimate(self, data, sampling_rate_hz):
+        if dataset_helper.is_multi_sensor_dataset(data):
+            raise NotImplementedError("Multisensor input is not supported yet")
+
+        if not dataset_helper.is_single_sensor_dataset(data):
+            raise ValueError("Provided data set is not supported by gaitmap")
+
         # TODO: is this way of using vel_axis_names OK?
-        self.velocity = pd.DataFrame(index=ForwardBackwardIntegration.vel_axis_names)
-        integral_forward = integrate.cumtrapz(data[SF_ACC], axis=0) / self.sampling_rate_hz
-        integral_backward = integrate.cumtrapz(np.flipud(data[SF_ACC]), axis=0) / self.sampling_rate_hz
+        self.velocity_ = pd.DataFrame(index=ForwardBackwardIntegration.vel_axis_names)
+        self.sampling_rate_hz = sampling_rate_hz
 
+        # TODO: make it possible to set initial value of integral from outside?
+        integral_forward = integrate.cumtrapz(data[SF_ACC], axis=0, initial=0) / self.sampling_rate_hz
+        integral_backward = integrate.cumtrapz(np.flipud(data[SF_ACC]), axis=0, initial=0) / self.sampling_rate_hz
         weights_vel = self._get_weight_matrix(data[SF_ACC])
-        self.velocity = pd.DataFrame(
+
+        self.velocity_ = pd.DataFrame(
             integral_forward * weights_vel + integral_backward * (1 - weights_vel),
             columns=ForwardBackwardIntegration.vel_axis_names,
             index=data.index,
@@ -52,9 +66,12 @@ class ForwardBackwardIntegration(BasePositionEstimation):
         # TODO: implement integration of velocity to obtain position
         return self
 
-    def _get_weight_matrix(self, data_to_integrate):
-        x = np.linspace(0, 1, len(data_to_integrate))
-        s = 1 / (1 + np.exp(-(x - self.x0)))
+    def _get_weight_matrix(self, data_to_integrate: pd.DataFrame):
+        n_samples = data_to_integrate.shape[0]
+        n_axes = data_to_integrate.shape[1]
+
+        x = np.linspace(0, 1, n_samples)
+        s = 1 / (1 + np.exp(-(x - self.turning_point) / self.steepness))
         weights = (s - s[0]) / (s[-1] - s[0])
 
-        return np.tile(weights, 3).reshape(len(data_to_integrate), 3)
+        return np.tile(weights, n_axes).reshape(n_samples, n_axes)
--- tests/test_orientation_estimation/test_position.py
+++ tests/test_orientation_estimation/test_position.py
@@ -1,3 +1,59 @@
+import numpy as np
+import pandas as pd
+import pytest
+import scipy
+
+from gaitmap.trajectory_reconstruction.position import ForwardBackwardIntegration
+from gaitmap.utils.consts import SF_ACC, SF_COLS
+
+
 class TestForwardBackwardIntegration:
-    def test_estimate(self):
-        pass
+    # TODO implement tests for turning_point != 0.5?
+    def test_estimate_dummy_data(self):
+        # algorithm parameters
+        turning_point = 0.5
+        steepness = 0.08
+
+        # make point symmetrical fake data
+        # -> should have zero as output
+        # -> should give nearly the same result as complete forward or backward integration (if .5 as turning point)
+        dummy = np.linspace(0, 1, 1000)
+        dummy_data = np.concatenate((dummy, -np.flip(dummy)))
+        dummy_pd = pd.DataFrame(data=np.tile(dummy_data, 6).reshape(len(dummy) * 2, 6), columns=SF_COLS)
+        sampling_frequency_hz = 100
+
+        position = ForwardBackwardIntegration(turning_point, steepness)
+        position.estimate(dummy_pd, sampling_frequency_hz)
+
+        # is the result nearly equal to zero?
+        np.testing.assert_array_almost_equal([0, 0, 0], position.velocity_.iloc[-1], decimal=2)
+
+        # is the result nearly the same as forward integration?
+        np.testing.assert_array_almost_equal([0, 0, 0], scipy.integrate.cumtrapz(dummy_data)[-1])
+
+        # is the result nearly the same as backward integration?
+        np.testing.assert_array_almost_equal([0, 0, 0], scipy.integrate.cumtrapz(np.flip(dummy_data))[-1])
+
+    def test_single_sensor_input(self, healthy_example_imu_data, healthy_example_stride_borders):
+        """Dummy test to see if the algorithm is generally working on the example data"""
+        # TODO add assert statement / regression test to check against previous result
+        data_left = healthy_example_imu_data["left_sensor"]
+        position = ForwardBackwardIntegration(0.5, 0.08)
+        position.estimate(data_left, 204.8)
+
+        return None
+
+    def test_estimate_multiple_sensors_input(self, healthy_example_imu_data):
+        # TODO: change as soon as multi-sensor is implemented
+        """Test if error is raised correctly on invalid input data type"""
+        data = healthy_example_imu_data
+        position = ForwardBackwardIntegration(0.5, 0.08)
+        with pytest.raises(NotImplementedError, match=r"Multisensor input is not supported yet"):
+            position.estimate(data, 204.8)
+
+    def test_estimate_valid_input_data(self):
+        """Test if error is raised correctly on invalid input data type"""
+        data = pd.DataFrame({"a": [0, 1, 2], "b": [3, 4, 5]})
+        position = ForwardBackwardIntegration(0.5, 0.08)
+        with pytest.raises(ValueError, match=r"Provided data set is not supported by gaitmap"):
+            position.estimate(data, 204.8)
