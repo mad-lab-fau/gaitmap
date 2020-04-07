b62a6abab3afca7032d402d5cea444d26d28fe36
Malte Ollenschlaeger <malte.ollenschlaeger@fau.de>
2020-04-07 09:19:26 +0200

    add tests for data types

--- gaitmap/trajectory_reconstruction/orientation_estimation.py
+++ gaitmap/trajectory_reconstruction/orientation_estimation.py
@@ -3,6 +3,7 @@ import pandas as pd
 from scipy.spatial.transform import Rotation
 
 from gaitmap.base import BaseOrientationEstimation
+from gaitmap.utils import dataset_helper
 from gaitmap.utils.consts import SF_GYR
 
 
@@ -36,7 +37,7 @@ class GyroIntegration(BaseOrientationEstimation):
 
     """
 
-    sensor_data: pd.DataFrame
+    data: pd.DataFrame
     estimated_orientations_: list
     sampling_rate_hz: float
     initial_orientation: Rotation
@@ -44,12 +45,12 @@ class GyroIntegration(BaseOrientationEstimation):
     def __init__(self, initial_orientation: Rotation):
         self.initial_orientation = initial_orientation
 
-    def estimate(self, sensor_data: pd.DataFrame, sampling_rate_hz: float):
+    def estimate(self, data: pd.DataFrame, sampling_rate_hz: float):
         """Use the initial rotation and the gyroscope signal to estimate rotations of second until last sample.
 
         Parameters
         ----------
-        sensor_data
+        data
             At least must contain 3D-gyroscope data for one sensor (multiple sensors not yet implemented).
         sampling_rate_hz
             Sampling rate with which gyroscopic data was recorded.
@@ -62,7 +63,7 @@ class GyroIntegration(BaseOrientationEstimation):
 
         Examples
         --------
-        >>> gyr_integrator.estimate(sensor_data, 204.8)
+        >>> gyr_integrator.estimate(data, 204.8)
         >>> orientations = gyr_integrator.estimated_orientations_
         >>> orientations[-1].as_quat()
         array([0., 1, 0., 0.])
@@ -70,11 +71,18 @@ class GyroIntegration(BaseOrientationEstimation):
         """
         # TODO: so far it is only possible to pass one sensor with columns being gyr_x...(and possilby others acc_z)
         #  ==> adapt to multiple sensors
+
+        if dataset_helper.is_multi_sensor_dataset(data):
+            raise NotImplementedError("Multisensor input is not supported yet")
+
+        if not dataset_helper.is_single_sensor_dataset(data):
+            raise ValueError("Provided data set is not supported by gaitmap")
+
         self.sampling_rate_hz = sampling_rate_hz
         self.estimated_orientations_ = []
         self.estimated_orientations_.append(self.initial_orientation)
-        for i_sample in range(1, len(sensor_data) + 1):
+        for i_sample in range(1, len(data) + 1):
             previous_quat = self.estimated_orientations_[i_sample - 1]
-            update_quat = Rotation.from_rotvec(sensor_data[SF_GYR].iloc[i_sample - 1] / self.sampling_rate_hz)
+            update_quat = Rotation.from_rotvec(data[SF_GYR].iloc[i_sample - 1] / self.sampling_rate_hz)
             self.estimated_orientations_.append(previous_quat * update_quat)
         return self
--- tests/test_orientation_estimation/test_orientation_estimation.py
+++ tests/test_orientation_estimation/test_orientation_estimation.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pytest
 from scipy.spatial.transform import Rotation
 
-from gaitmap.utils.consts import SF_GYR, SF_ACC
+from gaitmap.utils.consts import SF_COLS, SF_GYR
 from gaitmap.trajectory_reconstruction.orientation_estimation import GyroIntegration
 
 # TODO: @to08kece @Arne, add metatest once DTW is merged
@@ -14,7 +14,7 @@ class TestGyroIntegration:
         "axis_to_rotate, vector_to_rotate, expected_result",
         ((0, [0, 0, 1], [0, 0, -1]), (1, [0, 0, 1], [0, 0, -1]), (2, [1, 0, 0], [-1, 0, 0])),
     )
-    def test_180_x(self, axis_to_rotate: int, vector_to_rotate: list, expected_result: list):
+    def test_180(self, axis_to_rotate: int, vector_to_rotate: list, expected_result: list):
         """Rotate by 180 degree around one axis and check resulting rotation by transforming a 3D vector with start
         and final rotation.
 
@@ -30,7 +30,7 @@ class TestGyroIntegration:
             the result that is to be expected
 
         """
-        sensor_data = pd.DataFrame(columns=SF_GYR)
+        sensor_data = pd.DataFrame(columns=SF_COLS)
         fs = 100
 
         # 180 degree rotation around i_axis
@@ -40,7 +40,7 @@ class TestGyroIntegration:
             else:
                 sensor_data[i_axis] = [0] * fs
 
-        # create initial "spatial quaternion" that describes a vector along z-axis
+         # create initial "spatial quaternion" that describes a vector along z-axis
         if axis_to_rotate == 0 or axis_to_rotate == 1:
             rot_init = Rotation([0, 0, 1, 0])
         else:
@@ -50,3 +50,29 @@ class TestGyroIntegration:
         gyr_integrator.estimate(sensor_data, fs)
         rot_final = gyr_integrator.estimated_orientations_[-1]
         np.testing.assert_array_almost_equal(rot_final.apply(vector_to_rotate), expected_result)
+
+    def test_single_sensor_input(self, healthy_example_imu_data):
+        """Dummy test to see if the algorithm is generally working on the example data"""
+        # TODO add assert statement / regression test to check against previous result
+        data_left = healthy_example_imu_data["left_sensor"]
+        gyr_int = GyroIntegration(Rotation([0, 0, 0, 1]))
+        gyr_int.estimate(data_left, 204.8)
+
+        return None
+
+    def test_multiple_sensor_input(self, healthy_example_imu_data):
+        """Dummy test to see if the algorithm is generally working on the example data"""
+        # TODO add assert statement / regression test to check against previous result
+        data = healthy_example_imu_data
+        gyr_int = GyroIntegration(Rotation([0, 0, 0, 1]))
+        with pytest.raises(NotImplementedError, match=r"Multisensor input is not supported yet"):
+            gyr_int.estimate(data, 204.8)
+
+        return None
+
+    def test_valid_input_data(self):
+        """Test if error is raised correctly on invalid input data type"""
+        data = pd.DataFrame({"a": [0, 1, 2], "b": [3, 4, 5]})
+        gyr_int = GyroIntegration(Rotation([0, 0, 0, 1]))
+        with pytest.raises(ValueError, match=r"Provided data set is not supported by gaitmap"):
+            gyr_int.estimate(data, 204.8)
