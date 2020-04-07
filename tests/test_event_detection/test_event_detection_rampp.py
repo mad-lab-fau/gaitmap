87b1cf9293f31d09548ba1c5a61d7214b181bc43
Martin Ullrich <martin.ullrich@fau.de>
2020-04-07 08:10:34 +0200

    changed wind_size to win_size

--- tests/test_event_detection/test_event_detection_rampp.py
+++ tests/test_event_detection/test_event_detection_rampp.py
@@ -28,19 +28,19 @@ class TestEventDetectionRampp:
         with pytest.raises(ValueError, match=r"Provided data set is not supported by gaitmap"):
             ed.detect(data, 204.8, healthy_example_stride_borders)
 
-    def test_min_vel_search_wind_size_dummy_data(self):
+    def test_min_vel_search_win_size_dummy_data(self):
         """Test if error is raised correctly if windows size matches the size of the input data"""
         dummy_gyr = np.ones((100, 3))
-        with pytest.raises(ValueError, match=r"The value chosen for min_vel_search_wind_size is too large*"):
+        with pytest.raises(ValueError, match=r"The value chosen for min_vel_search_win_size is too large*"):
             _detect_min_vel(dummy_gyr, dummy_gyr.size)
 
-    def test_valid_min_vel_search_wind_size(self, healthy_example_imu_data, healthy_example_stride_borders):
-        """Test if error is raised correctly on too large min_vel_search_wind_size"""
+    def test_valid_min_vel_search_win_size(self, healthy_example_imu_data, healthy_example_stride_borders):
+        """Test if error is raised correctly on too large min_vel_search_win_size"""
         data_left = healthy_example_imu_data["left_sensor"]
         data_left.columns = BF_COLS
         stride_list_left = healthy_example_stride_borders["left_sensor"]
-        ed = RamppEventDetection(min_vel_search_wind_size=5000)
-        with pytest.raises(ValueError, match=r"The value chosen for min_vel_search_wind_size is too large*"):
+        ed = RamppEventDetection(min_vel_search_win_size=5000)
+        with pytest.raises(ValueError, match=r"The value chosen for min_vel_search_win_size is too large*"):
             ed.detect(data_left, 204.8, stride_list_left)
 
     def test_input_stride_list_size_one(self, healthy_example_imu_data, healthy_example_stride_borders):
