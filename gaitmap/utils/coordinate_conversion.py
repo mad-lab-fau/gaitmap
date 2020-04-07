788c1cf52f87c4373358ef8bb92e677a94a6a4df
Felix Kluge <felix.kluge@fau.de>
2020-04-07 09:15:31 +0200

    Reformat according to linting

--- gaitmap/utils/coordinate_conversion.py
+++ gaitmap/utils/coordinate_conversion.py
@@ -1,14 +1,13 @@
-"""A set of helper functions for the conversion of accelerometer and gyroscope data from the sensor to the body frame Definitions can be found in http://newgaitpipeline.mad-pages.informatik.uni-erlangen.de/gaitmap/guides/Coordinate-Systems.html."""
+"""A set of helper functions for the conversion of accelerometer and gyroscope data from the sensor to the body frame
+Definitions can be found in
+http://newgaitpipeline.mad-pages.informatik.uni-erlangen.de/gaitmap/guides/Coordinate-Systems.html."""
 
 from typing import Optional, List
 
 import pandas as pd
 
 from gaitmap.utils.consts import SF_COLS, BF_COLS
-from gaitmap.utils.dataset_helper import (
-    Dataset,
-    is_multi_sensor_dataset
-)
+from gaitmap.utils.dataset_helper import Dataset, is_multi_sensor_dataset
 
 
 def convert_left_foot_to_bf(data: pd.DataFrame):
