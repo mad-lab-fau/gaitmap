d0b74029b4be9696e06f027aca0512f927e45b79
Felix Kluge <felix.kluge@fau.de>
2020-04-07 09:00:02 +0200

    Update tests after reformatting

--- tests/test_utils/test_coordinate_conversion.py
+++ tests/test_utils/test_coordinate_conversion.py
@@ -3,7 +3,7 @@ import pytest
 from pandas.testing import assert_frame_equal
 from gaitmap.utils.consts import SF_COLS, BF_COLS
 
-from gaitmap.utils.coordinate_conversion import convert_left_foot, convert_right_foot, rotate
+from gaitmap.utils.coordinate_conversion import convert_left_foot_to_bf, convert_right_foot_to_bf, convert_to_bf
 
 
 class TestConvertAxes:
@@ -25,21 +25,23 @@ class TestConvertAxes:
         self.data_df_expected = pd.concat(self.data_dict_expected, axis=1)
 
     def test_convert_left_foot(self):
-        data_converted = convert_left_foot(self.data_left)
+        data_converted = convert_left_foot_to_bf(self.data_left)
         assert_frame_equal(data_converted, self.data_left_expected)
 
     def test_convert_right_foot(self):
-        data_converted = convert_right_foot(self.data_right)
+        data_converted = convert_right_foot_to_bf(self.data_right)
         assert_frame_equal(data_converted, self.data_right_expected)
 
     def test_rotate_multisensor(self):
-        data_converted = rotate(self.data_df, left=["left_sensor"], right=["right_sensor"])
+        data_converted = convert_to_bf(self.data_df, left=["left_sensor"], right=["right_sensor"])
         assert_frame_equal(data_converted, self.data_df_expected)
 
     def test_rotate_multisensor_left(self):
-        data_converted = rotate(self.data_df, left=["left_sensor"])
+        data_converted = convert_to_bf(self.data_df, left=["left_sensor"])
         assert_frame_equal(data_converted["left_sensor"], self.data_left_expected)
 
     def test_rotate_multisensor_right(self):
-        data_converted = rotate(self.data_df, right=["right_sensor"])
+        data_converted = convert_to_bf(self.data_df, right=["right_sensor"])
         assert_frame_equal(data_converted["right_sensor"], self.data_right_expected)
+
+
