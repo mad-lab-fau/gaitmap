29d07d4bb34094f347610b5ab8c0f6a314b7394d
Malte Ollenschlaeger <malte.ollenschlaeger@fau.de>
2020-04-07 10:55:12 +0200

    implement calculation of forward and backward integral

--- gaitmap/trajectory_reconstruction/position.py
+++ gaitmap/trajectory_reconstruction/position.py
@@ -1,5 +1,9 @@
 """Estimation of velocity and position relative to first sample of passed data."""
+import numpy as np
+import pandas as pd
+
 from gaitmap.base import BasePositionEstimation
+from gaitmap.utils.consts import SF_ACC
 
 
 class ForwardBackwardIntegration(BasePositionEstimation):
@@ -10,8 +14,20 @@ class ForwardBackwardIntegration(BasePositionEstimation):
     axis (=z-axis or superior-inferior-axis, see :ref:`ff`.
     """
 
+    # TODO: add support for multiple sensors
+    sampling_rate_hz: float
+    # TODO: change name of volumns to 'vel_axis' and 'pos_axis'
+
+    velocity = pd.DataFrame(SF_ACC)
+    position = pd.DataFrame(SF_ACC)
+
     def __init__(self, parameter):
         self.parameter = parameter
 
     def estimate(self, data):
+        integral_forward = np.cumtrapz(data[SF_ACC]) / self.sampling_rate_hz
+        integral_backward = np.cumtrapz(np.flipud(data[SF_ACC])) / self.sampling_rate_hz
+        pass
+
+    def _get_weighting_function(self,):
         pass
