3c72dfacdcf5873edcd33df61f91b51763e5f653
Arne Küderle <arne.kuederle@fau.de>
2020-04-17 20:52:26 +0200

    Imported spatial parameters in init

--- gaitmap/parameters/__init__.py
+++ gaitmap/parameters/__init__.py
@@ -1,4 +1,5 @@
 """Calculate biomechanical gait parameters based on all the information calculated in the rest of the pipeline."""
 from gaitmap.parameters.temporal_parameters import TemporalParameterCalculation
+from gaitmap.parameters.spatial_parameters import SpatialParameterCalculation
 
-__all__ = ["TemporalParameterCalculation"]
+__all__ = ["TemporalParameterCalculation", "SpatialParameterCalculation"]
