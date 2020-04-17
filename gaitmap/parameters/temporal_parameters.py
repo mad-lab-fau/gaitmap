0164589a1c8c13a8b5c5d8d86607582f623a0d3b
Arne Küderle <arne.kuederle@fau.de>
2020-04-17 21:49:07 +0200

    Reordered methods

--- gaitmap/parameters/temporal_parameters.py
+++ gaitmap/parameters/temporal_parameters.py
@@ -37,6 +37,32 @@ class TemporalParameterCalculation(BaseTemporalParameterCalculation):
     sampling_rate_hz: float
     stride_event_list: StrideList
 
+    def calculate(self: BaseType, stride_event_list: StrideList, sampling_rate_hz: float) -> BaseType:
+        """Find temporal parameters of all strides after segmentation and detecting events for all sensors.
+
+        Parameters
+        ----------
+        stride_event_list
+            Gait events for each stride obtained from event detection
+        sampling_rate_hz
+            The sampling rate of the data signal.
+
+        Returns
+        -------
+        self
+            The class instance with temporal parameters populated in parameters_
+
+        """
+        self.sampling_rate_hz = sampling_rate_hz
+        self.stride_event_list = stride_event_list
+        if is_single_sensor_stride_list(stride_event_list, stride_type="min_vel"):  # this means single sensor
+            self.parameters_ = self._calculate_single_sensor(stride_event_list, sampling_rate_hz)
+        elif is_multi_sensor_stride_list(stride_event_list, stride_type="min_vel"):
+            self.parameters_ = self._calculate_multiple_sensor(stride_event_list, sampling_rate_hz)
+        else:
+            raise ValueError("Stride list datatype is not supported.")
+        return self
+
     @staticmethod
     def _calculate_single_sensor(stride_event_list: SingleSensorStrideList, sampling_rate_hz: float) -> pd.DataFrame:
         """Find temporal parameters  of each stride in case of single sensor.
@@ -92,38 +118,12 @@ class TemporalParameterCalculation(BaseTemporalParameterCalculation):
             parameters_[sensor] = self._calculate_single_sensor(stride_event_list[sensor], sampling_rate_hz)
         return parameters_
 
-    def calculate(self: BaseType, stride_event_list: StrideList, sampling_rate_hz: float) -> BaseType:
-        """Find temporal parameters of all strides after segmentation and detecting events for all sensors.
-
-        Parameters
-        ----------
-        stride_event_list
-            Gait events for each stride obtained from event detection
-        sampling_rate_hz
-            The sampling rate of the data signal.
-
-        Returns
-        -------
-        self
-            The class instance with temporal parameters populated in parameters_
-
-        """
-        self.sampling_rate_hz = sampling_rate_hz
-        self.stride_event_list = stride_event_list
-        if is_single_sensor_stride_list(stride_event_list, stride_type="min_vel"):  # this means single sensor
-            self.parameters_ = self._calculate_single_sensor(stride_event_list, sampling_rate_hz)
-        elif is_multi_sensor_stride_list(stride_event_list, stride_type="min_vel"):
-            self.parameters_ = self._calculate_multiple_sensor(stride_event_list, sampling_rate_hz)
-        else:
-            raise ValueError("Stride list datatype is not supported.")
-        return self
-
 
 def _calc_time_stamp_(ic_event: float, sampling_rate_hz: float) -> float:
     return ic_event / sampling_rate_hz
 
 
-def calc_stride_time(ic_event: float, pre_ic_event: float, sampling_rate_hz: float) -> float:
+def calc_stride_time(ic_event: pd.Series, pre_ic_event: pd.Series, sampling_rate_hz: float) -> pd.Series:
     """Find stride time.
 
     Parameters
