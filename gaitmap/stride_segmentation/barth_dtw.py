6cc722742ae7160a0ded2fbc3ca72b07b254e5d0
Arne Küderle <arne.kuederle@fau.de>
2020-04-21 14:34:01 +0200

    Updated the default snap width

--- gaitmap/stride_segmentation/barth_dtw.py
+++ gaitmap/stride_segmentation/barth_dtw.py
@@ -124,9 +124,9 @@ class BarthDtw(BaseDtw):
         find_matches_method: Literal["min_under_thres", "find_peaks"] = "find_peaks",
         max_cost: Optional[float] = 2000.0,
         min_match_length_s: Optional[float] = 0.6,
-        snap_to_min_win_ms: Optional[float] = 100,
+        snap_to_min_win_ms: Optional[float] = 300,
         snap_to_min_axis: Optional[str] = "gyr_ml",
-        conflict_resolution: bool = True
+        conflict_resolution: bool = True,
     ):
         self.snap_to_min_win_ms = snap_to_min_win_ms
         self.snap_to_min_axis = snap_to_min_axis
