eee3ccc97d3d91f12639e0b6cf262e53c4466c2a
Arne Küderle <arne.kuederle@fau.de>
2020-04-24 13:44:02 +0200

    Added test for dtw example

--- examples/barth_dtw_stride_segmentation.py
+++ examples/barth_dtw_stride_segmentation.py
@@ -73,7 +73,9 @@ dtw = dtw.segment(data=bf_data, sampling_rate_hz=sampling_rate_hz)
 # ----------------------
 # The main output is the `stride_list_`, which contains the start and the end of all identified strides.
 # As we passed a dataset with two sensors, the output will a dictionary.
-dtw.stride_list_["left_sensor"].head()
+stride_list_left = dtw.stride_list_["left_sensor"]
+print("{} strides were detected.".format(len(stride_list_left)))
+stride_list_left.head()
 
 # %%
 # To get a better understanding of the results, we can plot additional information about the results.
