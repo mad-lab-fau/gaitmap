2d5c0a51d48b2c9c9930bca1bab71b421548d703
Arne Küderle <arne.kuederle@fau.de>
2020-05-03 15:58:35 +0200

    Added tests for initial orientation

--- tests/test_trajectory_reconstruction/test_stride_level_trajectory.py
+++ tests/test_trajectory_reconstruction/test_stride_level_trajectory.py
@@ -1,7 +1,13 @@
+import numpy as np
+import pandas as pd
 import pytest
+from numpy.testing import assert_array_equal
+from scipy.spatial.transform import Rotation
 
 from gaitmap.base import BaseType
 from gaitmap.trajectory_reconstruction.stride_level_trajectory import StrideLevelTrajectory
+from gaitmap.utils.consts import SF_COLS
+from gaitmap.utils.rotations import find_angle_between_orientations
 from tests.mixins.test_algorithm_mixin import TestAlgorithmMixin
 
 
@@ -18,3 +24,25 @@ class TestMetaFunctionality(TestAlgorithmMixin):
             sampling_rate_hz=1,
         )
         return trajectory
+
+
+class TestInitCalculation:
+    """Test the calculation of initial rotations per stride.
+
+    No complicated tests here, as this uses `get_gravity_rotation`, which is well tested
+    """
+
+    def test_calc_initial_dummy(self):
+        """No rotation expected as already aligned."""
+        dummy_data = pd.DataFrame(np.repeat(np.array([0, 0, 1, 0, 0, 0])[None, :], 20, axis=0), columns=SF_COLS)
+        start_ori = StrideLevelTrajectory.calculate_initial_orientation(dummy_data, 10, 8)
+        assert_array_equal(start_ori.as_quat(), Rotation.identity().as_quat())
+
+    @pytest.mark.parametrize("start", [0, 99])
+    def test_start_of_stride_equals_start_or_end_of_data(self, start):
+        """If start is to close to the start or the end of the data a warning is emitted."""
+        dummy_data = pd.DataFrame(np.repeat(np.array([0, 0, 1, 0, 0, 0])[None, :], 100, axis=0), columns=SF_COLS)
+        with pytest.warns(UserWarning) as w:
+            StrideLevelTrajectory.calculate_initial_orientation(dummy_data, start, 8)
+
+        assert "complete window length" in str(w[0])
