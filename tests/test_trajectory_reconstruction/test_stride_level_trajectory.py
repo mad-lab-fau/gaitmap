4a731df6ba61041b082c224caf12cef16057edb5
Arne Küderle <arne.kuederle@fau.de>
2020-05-04 09:28:54 +0200

    Linting

--- tests/test_trajectory_reconstruction/test_stride_level_trajectory.py
+++ tests/test_trajectory_reconstruction/test_stride_level_trajectory.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import pytest
 from numpy.testing import assert_array_equal
-from pandas._testing import assert_frame_equal
 from scipy.spatial.transform import Rotation
 
 from gaitmap.base import BaseType
