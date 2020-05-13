9998cfd1caa2c611b52a4a477772b28c9637e92a
Matthias Zuerl <Matthias.Zuerl@fau.de>
2020-05-13 21:19:29 +0200

    created new file for top-to-bottom pipeline example

---
+++ examples/top_to_bottom.py
@@ -0,0 +1,8 @@
+r"""
+Top to bottom example
+=============
+
+This example whole top-to-bottom pipeline: pre-processing -> DTW -> EventDetection -> TrajectoryReconstruction ->
+ParameterEstimation.
+"""
+
