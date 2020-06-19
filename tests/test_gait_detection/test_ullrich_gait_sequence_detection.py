82d94178480cf590776a740d20e05f3be3244c56
Martin Ullrich <martin.ullrich@fau.de>
2020-06-19 16:06:36 +0200

    added snapshot testing

--- tests/test_gait_detection/test_ullrich_gait_sequence_detection.py
+++ tests/test_gait_detection/test_ullrich_gait_sequence_detection.py
@@ -20,9 +20,18 @@ class TestUllrichGaitSequenceDetection:
         gsd = UllrichGaitSequenceDetection()
         gsd = gsd.detect(data, 204.8)
 
+        assert len(gsd.gait_sequences_["left_sensor"]) == 1
+        assert len(gsd.gait_sequences_["right_sensor"]) == 1
+
         return None
 
-    def test_different_activities(self, healthy_example_imu_data, snapshot):
+    @pytest.mark.parametrize(
+        "sensor_channel_config,peak_prominence", (("gyr_ml", 17), ("acc_si", 8), (BF_ACC, 13), (BF_GYR, 11))
+    )
+    def test_different_activities_different_configs(self, healthy_example_imu_data, sensor_channel_config,
+                                                 peak_prominence, snapshot):
+        """Test if the algorithm is generally working with different sensor channel configs and their respective
+                optimal peak prominence thresholds."""
         data = coordinate_conversion.convert_to_fbf(
             healthy_example_imu_data, left=["left_sensor"], right=["right_sensor"]
         )
@@ -44,27 +53,14 @@ class TestUllrichGaitSequenceDetection:
 
         test_data_df = pd.concat([rest_df, data, non_gait_df, data, rest_df], ignore_index=True)
 
-        gsd = UllrichGaitSequenceDetection()
+        gsd = UllrichGaitSequenceDetection(sensor_channel_config=sensor_channel_config, peak_prominence=peak_prominence)
         gsd = gsd.detect(test_data_df, 204.8)
 
-        return None
-
-    @pytest.mark.parametrize(
-        "sensor_channel_config,peak_prominence", (("gyr_ml", 17), ("acc_si", 8), (BF_ACC, 13), (BF_GYR, 11))
-    )
-    def test_different_sensor_channel_configs_with_peak_prominence(
-        self, healthy_example_imu_data, snapshot, sensor_channel_config, peak_prominence
-    ):
-        """Test if the algorithm is generally working with different sensor channel configs and their respective
-        optimal peak prominence thresholds."""
-        data = coordinate_conversion.convert_to_fbf(
-            healthy_example_imu_data, left=["left_sensor"], right=["right_sensor"]
-        )
-
-        gsd = UllrichGaitSequenceDetection(sensor_channel_config=sensor_channel_config, peak_prominence=peak_prominence)
-        gsd = gsd.detect(data, 204.8)
-
-        return None
+        filename = sensor_channel_config
+        if isinstance(filename, (tuple, list)):
+            filename = "_".join(filename)
+        filename = filename + "_" + str(peak_prominence)
+        snapshot.assert_match(gsd.gait_sequences_["left_sensor"], filename, check_dtype=True)
 
     def test_invalid_sensor_channel_config_type(self, healthy_example_imu_data):
         """Check if ValueError is raised for wrong sensor_channel_config data type."""
