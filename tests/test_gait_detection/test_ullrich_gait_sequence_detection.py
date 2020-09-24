0f03b823ceddd1a418c12612da9734a9beabcbd2
Martin Ullrich <martin.ullrich@fau.de>
2020-09-24 11:04:40 +0200

    added a few tests for edge cases

--- tests/test_gait_detection/test_ullrich_gait_sequence_detection.py
+++ tests/test_gait_detection/test_ullrich_gait_sequence_detection.py
@@ -72,6 +72,60 @@ class TestUllrichGaitSequenceDetection:
         filename = filename + "_" + str(peak_prominence)
         snapshot.assert_match(gsd.gait_sequences_["left_sensor"], filename, check_dtype=False)
 
+    def test_signal_length_one_window_size(self, healthy_example_imu_data, snapshot):
+        """Test to see if the algorithm is working if the signal length equals to one window size"""
+        data = coordinate_conversion.convert_to_fbf(
+            healthy_example_imu_data, left=["left_sensor"], right=["right_sensor"]
+        )
+
+        data = data["left_sensor"]
+
+        sampling_rate_hz = 204.8
+        window_size_s = 10
+        data = data.iloc[2048 : 2048 + int(window_size_s * sampling_rate_hz)]
+
+        gsd = UllrichGaitSequenceDetection(window_size_s=window_size_s)
+        gsd = gsd.detect(data, 204.8)
+
+        assert len(gsd.gait_sequences_) == 1
+
+    def test_on_signal_without_activity(self, snapshot):
+        """Test to see if the algorithm is working if the signal contains no activity at all"""
+
+        # TODO can we get all columns for a dataset easier than like this?
+        data_columns = [col for sublist in [BF_ACC, BF_GYR] for col in sublist]
+
+        # induce rest
+        rest_df = pd.DataFrame([[0] * 6], columns=data_columns)
+        rest_df = pd.concat([rest_df] * 2048)
+
+        gsd = UllrichGaitSequenceDetection()
+        gsd = gsd.detect(rest_df, 204.8)
+
+        assert len(gsd.gait_sequences_) == 0
+
+    def test_on_signal_with_only_nongait(self, snapshot):
+        """Test to see if the algorithm is working if the signal contains only non-gait activity"""
+
+        # TODO can we get all columns for a dataset easier than like this?
+        data_columns = [col for sublist in [BF_ACC, BF_GYR] for col in sublist]
+
+        # induce non-gait cyclic activity
+        # create a sine signal to mimic non-gait
+        sampling_rate = 204.8
+        samples = 2048
+        t = np.arange(samples) / sampling_rate
+        freq = 1
+        test_signal = np.sin(2 * np.pi * freq * t) * 200
+
+        test_signal_reshaped = np.tile(test_signal, (6, 1)).T
+        non_gait_df = pd.DataFrame(test_signal_reshaped, columns=data_columns)
+
+        gsd = UllrichGaitSequenceDetection()
+        gsd = gsd.detect(non_gait_df, 204.8)
+
+        assert len(gsd.gait_sequences_) == 0
+
     def test_invalid_sensor_channel_config_type(self, healthy_example_imu_data):
         """Check if ValueError is raised for wrong sensor_channel_config data type."""
         data = coordinate_conversion.convert_to_fbf(
