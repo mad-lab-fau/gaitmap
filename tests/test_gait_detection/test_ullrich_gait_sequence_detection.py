200f42c6ffaaba4f1c8185306a07ed254ef8396f
Martin Ullrich <martin.ullrich@fau.de>
2020-09-24 12:40:34 +0200

    added test for row_wise_autocorrelation

--- tests/test_gait_detection/test_ullrich_gait_sequence_detection.py
+++ tests/test_gait_detection/test_ullrich_gait_sequence_detection.py
@@ -3,7 +3,8 @@ import pandas as pd
 import pytest
 
 from gaitmap.gait_detection import UllrichGaitSequenceDetection
-from gaitmap.utils import coordinate_conversion
+from gaitmap.utils import coordinate_conversion, signal_processing
+from gaitmap.utils.consts import BF_ACC, BF_GYR
 
 
 class TestUllrichGaitSequenceDetection:
@@ -218,3 +219,26 @@ class TestUllrichGaitSequenceDetection:
         with pytest.raises(ValueError, match=r".* synchronized data sets."):
             gsd = UllrichGaitSequenceDetection(merge_gait_sequences_from_sensors=merge_gait_sequences_from_sensors)
             gsd = gsd.detect(data_dict, 204.8)
+
+    def test_row_wise_autocorrelation(self):
+        """Test if the manually implemented row wise autocorrelation function produces similar results than the numpy
+         implementation"""
+
+        # create a test signal
+        sampling_rate = 204.8
+        samples = 100
+        t = np.arange(samples) / sampling_rate
+        freq = 1
+        test_signal_sin = np.sin(2 * np.pi * freq * t)
+        test_signal_cos = np.cos(2 * np.pi * freq * t)
+        test_signal_array = np.array([test_signal_sin, test_signal_cos])
+
+        max_lag = 99
+        autocorr_row_wise = signal_processing.row_wise_autocorrelation(test_signal_array, max_lag)
+
+        for idx, test_signal in enumerate([test_signal_sin, test_signal_cos]):
+            autocorr_1d = np.correlate(test_signal, test_signal, mode="full")
+            autocorr_1d = autocorr_1d[autocorr_1d.size // 2 :][: max_lag + 1]
+
+            # for some reason the values are not exactly equal but difference is smaller than eps
+            np.testing.assert_allclose(autocorr_1d, autocorr_row_wise[idx])
