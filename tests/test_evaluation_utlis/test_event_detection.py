bbb49c5b29f2d61a4b008d992173ec2616bebe55
Raul Simpetru <raul.simpetru@fau.de>
2020-11-02 08:33:54 +0100

    Improve _create_valid_list in TestSpecialEvaluateStrideList

--- tests/test_evaluation_utlis/test_event_detection.py
+++ tests/test_evaluation_utlis/test_event_detection.py
@@ -10,18 +10,14 @@ from gaitmap.evaluation_utils.scores import _get_match_type_dfs
 
 class TestSpecialEvaluateStrideList:
     def _create_valid_list(self, labels, extra_columns=None):
+        columns = ["start", "end"]
+        
         if extra_columns:
-            if isinstance(extra_columns, str):
-                df = pd.DataFrame(labels, columns=["start", "end", extra_columns])
-            else:
-                columns = ["start", "end"]
-                for extra_column in extra_columns:
-                    columns.append(extra_column)
-
-                df = pd.DataFrame(labels, columns=columns)
-        else:
-            df = pd.DataFrame(labels, columns=["start", "end"])
+            columns += [extra_columns] if isinstance(extra_columns, str) else extra_columns
+            
+        df = pd.DataFrame(labels, columns=columns)
         df.index.name = "s_id"
+        
         return df
 
     def test_too_many_column_names(self):
