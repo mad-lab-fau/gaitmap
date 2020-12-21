9f639d6b8a7d6696b8b2c9169da2c6e5c9ce0ce9
Raul C. Sîmpetru <raul.simpetru@fau.de>
2020-12-21 18:33:08 +0100

    Make some improvements

--- gaitmap/future/dataset.py
+++ gaitmap/future/dataset.py
@@ -1,97 +1,70 @@
-from operator import itemgetter
-from typing import Union, Optional
+from typing import Optional
 
 import numpy as np
 import pandas as pd
 
+from gaitmap.base import _BaseSerializable
 
-class Dataset:
-    def __init__(self, index: Optional[Union[pd.Index, pd.MultiIndex]] = None, select_lvl: int = 0):
-        self.index = index
+
+class Dataset(_BaseSerializable):
+    def __init__(self, select_lvl: str, subset_index: Optional[pd.DataFrame] = None):
+        self.__subset_index = subset_index
         self.select_lvl = select_lvl
 
     @property
     def index(self):
-        return self._index
-
-    @index.setter
-    def index(self, value: Optional[Union[pd.Index, pd.MultiIndex]]):
-        if value is None:
-            self._index = self._create_index()
-        else:
-            if value.names.count(None) == len(value.names):
-                value.set_names(tuple(["index_lvl_{}".format(i) for i in range(len(value[0]))]), inplace=True)
-
-            self._index = value
+        if self.__subset_index is None:
+            return self._create_index()
+        return self.__subset_index
 
     @property
     def select_lvl(self):
         return self._select_lvl
 
     @select_lvl.setter
-    def select_lvl(self, value: int):
-        self._select_lvl = value
-        self.columns = None
+    def select_lvl(self, value: str):
+        if value in self.index.columns:
+            self._select_lvl = value
+        else:
+            raise ValueError("select_lvl must be one of {}".format(self.index.columns.to_list()))
 
     @property
     def columns(self):
-        if self.select_lvl == 0:
-            return self._columns.values
-
-        return list(set(map(itemgetter(self.select_lvl), self._columns.values)))
-
-    @columns.setter
-    def columns(self, _: None):
-        self._columns = pd.Series(
-            self.index.to_frame().reset_index(drop=True).groupby(self.index.names[: self.select_lvl + 1]).groups.keys()
-        )
+        return self.index.groupby(by=self.select_lvl).groups
 
     @property
     def shape(self):
-        return [len(self._columns)]
+        return [len(np.concatenate(list(self.columns.values())))]
 
     def __getitem__(self, subscript):
-        where_to_search = self.index.tolist()
-        filtered_list = []
+        to_concat = []
 
-        if isinstance(subscript, (tuple, np.ndarray)):
+        if isinstance(subscript, (tuple, list, np.ndarray)):
             if isinstance(subscript[0], str):
-                filtered_list = list(filter(lambda x: x[: self.select_lvl + 1][-1] in subscript, where_to_search))
+                for string in subscript:
+                    to_concat.append(self.index.loc[self.columns[string]])
             else:
-                what_to_search = (
-                    [tuple([x]) for x in self._columns.iloc[list(subscript)].to_list()]
-                    if self.select_lvl == 0
-                    else self._columns.iloc[list(subscript)].to_list()
+                to_concat.append(
+                    self.index.iloc[list(filter(lambda x: x in np.concatenate(list(self.columns.values())), subscript))]
                 )
 
-                filtered_list = list(filter(lambda x: x[: self.select_lvl + 1] in what_to_search, where_to_search))
-
         elif isinstance(subscript, str):
-            filtered_list = (
-                list(filter(lambda x: x[0] == subscript, where_to_search))
-                if self.select_lvl == 0
-                else list(filter(lambda x: x[: self.select_lvl + 1][-1] == subscript, where_to_search))
-            )
+            to_concat.append(self.index.loc[self.columns[subscript]])
 
-        if len(filtered_list) == 0:
+        if len(to_concat) == 0:
             raise IndexError("Subscript {} not applicable to this dataset!".format(subscript))
 
-        return Dataset(self._make_multi_index(filtered_list), select_lvl=self.select_lvl,)
+        return Dataset(self.select_lvl, pd.concat(to_concat).reset_index(drop=True))
 
     def __repr__(self):
         return str(self.index)
 
-    def to_multi_index(self):
+    def index_as_multi_index(self):
+        return pd.MultiIndex.from_frame(self.index)
+
+    def index_as_dataframe(self):
         return self.index
-
-    def to_frame(self):
-        return self.index.to_frame().reset_index(drop=True)
-
-    def _make_multi_index(self, value: list):
-        return pd.MultiIndex.from_arrays(
-            [list(map(itemgetter(i), value)) for i in range(len(value[0]))], names=self.index.names
-        )
 
     @staticmethod
     def _create_index():
-        raise ValueError("A dataset requires a pd.MultiIndex that it should represent!")
+        raise NotImplementedError
