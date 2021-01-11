13ee466c6017c378e006c6350073efe19ef03f07
Raul C. Sîmpetru <raul.simpetru@fau.de>
2021-01-11 19:44:07 +0100

    Make a few internal changes in dataset.py

--- gaitmap/future/dataset.py
+++ gaitmap/future/dataset.py
@@ -79,8 +79,8 @@ class Dataset(_BaseSerializable):
     """
 
     def __init__(self, subset_index: Optional[pd.DataFrame] = None, select_lvl: Optional[str] = None):
-        self.__subset_index = subset_index
-        self.select_lvl = list(self.index.columns)[0] if select_lvl is None else select_lvl
+        self._subset_index = subset_index
+        self.select_lvl = select_lvl
 
     @property
     def index(self):
@@ -88,16 +88,19 @@ class Dataset(_BaseSerializable):
         if hasattr(self, "__cached_index"):
             return self.__cached_index
 
-        return self.__create_index() if self.__subset_index is None else self.__subset_index
+        return self.__create_index() if self._subset_index is None else self._subset_index
 
     @property
-    def select_lvl(self):  # noqa: missing-function-docstring
+    def select_lvl(self):
+        """Get select_lvl."""
         return self._select_lvl
 
     @select_lvl.setter
-    def select_lvl(self, value: str):
+    def select_lvl(self, value: Optional[str]):
         """Set select_lvl."""
-        if value in self.index.columns:
+        if value is None:
+            self._select_lvl = list(self.index.columns)[0]
+        elif value in self.index.columns:
             self._select_lvl = value
         else:
             raise ValueError("select_lvl must be one of {}".format(self.index.columns.to_list()))
@@ -110,7 +113,7 @@ class Dataset(_BaseSerializable):
     @property
     def shape(self):
         """Get shape."""
-        return [len(np.concatenate(list(self.columns.values())))]
+        return (len(np.concatenate(list(self.columns.values()))),)
 
     def __getitem__(self, subscript):
         """Return a dataset object."""
@@ -146,27 +149,11 @@ class Dataset(_BaseSerializable):
         return self.index
 
     def __is_single(self):
-        not_zero = 0
-
-        for value in self.columns.values():
-            not_zero += 1 if len(value) > 0 else 0
-
-        return not_zero <= 1
+        return len([v for v in self.columns.values() if len(v) > 0]) <= 1
 
     def __iter__(self):
-        """Return same object and initialize private attributes for iteration."""
-        self.__n = 0  # noqa: attribute-defined-outside-init
-        self.__categories_to_search = self.index[self.select_lvl].cat.categories  # noqa: attribute-defined-outside-init
-        return self
-
-    def __next__(self):
-        """Return new dataset objects by splitting the initial object by the categories of the selected level."""
-        if self.__is_single() or self.__n == len(self.__categories_to_search):
-            raise StopIteration
-
-        to_return = self.__getitem__(self.__categories_to_search[self.__n])
-        self.__n += 1
-        return to_return
+        """Return generator object containing subset of every category from the selected level."""
+        return (self.__getitem__(category) for category in self.index[self.select_lvl].cat.categories)
 
     @staticmethod
     def __create_index():
