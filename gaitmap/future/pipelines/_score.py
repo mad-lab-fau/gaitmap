878a39f04e1a9186c5f0359cc2167e4db563590d
Arne Küderle <arne.kuederle@fau.de>
2021-03-23 17:45:18 +0100

    Added first version of core method for cross validate

--- gaitmap/future/pipelines/_score.py
+++ gaitmap/future/pipelines/_score.py
@@ -10,9 +10,11 @@ import time
 from typing import Dict, Optional
 
 import numpy as np
+from joblib import Memory
 from sklearn import clone
 
 from gaitmap.future.dataset import Dataset
+from gaitmap.future.pipelines import BaseOptimize
 from gaitmap.future.pipelines._pipelines import SimplePipeline
 from gaitmap.future.pipelines._scorer import GaitmapScorer, _ERROR_SCORE_TYPE
 
@@ -93,4 +95,88 @@ def _score(
         result["data_labels"] = dataset.groups
     if return_parameters:
         result["parameters"] = parameters
+    return result
+
+
+def _optimize_and_score(
+    optimizer: BaseOptimize,
+    dataset: Dataset,
+    scorer: GaitmapScorer,
+    train: np.ndarray,
+    test: np.ndarray,
+    hyperparameters: Optional[Dict],
+    pure_parameters: Optional[Dict],
+    return_train_score=False,
+    return_parameters=False,
+    return_data_labels=False,
+    return_times=False,
+    error_score: _ERROR_SCORE_TYPE = np.nan,
+    memory: Optional[Memory] = None,
+):
+    """Optimize and score the optimized pipeline on the train and test data, respectively.
+
+    This method is aware of the differences between hyperparameters and normal (pure) parameters.
+    This can be used to cache the results of training, as the training results should only depend on hyperparameters,
+    but not on normal parameters.
+    The provided `memory` instance is used to perform this caching step.
+
+    Note, that caching in this context should only be performed in the context of a single call to e.g. GridSearchCV
+    and the cache should be deleted afterwards to avoid cache leak.
+    Therefore, the cachedir should ideally be set to a random tmp dir by the caller.
+    """
+    if memory is None:
+        memory = Memory(None)
+    if hyperparameters is not None:
+        # clone after setting parameters in case any parameters are estimators (like pipeline steps).
+        cloned_hyperparameters = {}
+        for k, v in hyperparameters.items():
+            cloned_hyperparameters[k] = clone(v, safe=False)
+        hyperparameters = cloned_hyperparameters
+    if pure_parameters is not None:
+        cloned_pure_parameters = {}
+        for k, v in hyperparameters.items():
+            cloned_pure_parameters[k] = clone(v, safe=False)
+        pure_parameters = cloned_pure_parameters
+
+    train_set = dataset[train]
+    test_set = dataset[test]
+
+    # We do not set all paras right away, we first create a cached optimize function that only has the hyper
+    # parameters as input.
+    # This allows to cache the train results, if the _optimize_and_score is called multiple times with the same hyper
+    # parameters.
+    # To be sure that nothing "bad" happens here, we also pass in the pipeline itself to invalidate the cache,
+    # in case a completly different pipeline/algorithm is optimized.
+    # Ideally the `memory` object used here should only be used once in the context of e.g. a GridSearchCV.
+    # TODO: Throw error if optimization modifies pure parameter
+    def cachable_optimize(opti: BaseOptimize, hyperparas: Dict, data: Dataset) -> BaseOptimize:
+        return opti.set_params(**hyperparas).optimize(data)
+
+    start_time = time.time()
+    optimize_func = memory.cache(cachable_optimize)
+    optimizer = optimize_func(optimizer, hyperparameters, train_set)
+    optimize_time = time.time() - start_time
+
+    # Now we set the remaining paras
+    optimized_pipeline = optimizer.optimized_pipeline_.set_params(**pure_parameters)
+
+    agg_scores, single_scores = scorer(optimized_pipeline, test_set, error_score)
+    score_time = time.time() - optimize_time
+
+    result = {"scores": agg_scores, "single_scores": single_scores}
+    if return_train_score:
+        train_agg_scores, train_single_scores = scorer(optimized_pipeline, train_set, error_score)
+        train_score_time = time.time() - score_time
+        result["train_scores"] = train_agg_scores
+        result["train_single_scores"] = train_single_scores
+    if return_times:
+        result["score_time"] = score_time
+        result["optimize_time"] = optimize_time
+        if return_train_score:
+            result["train_score_time"] = train_score_time
+    if return_data_labels:
+        result["train_data_labels"] = train_set.groups
+        result["test_data_labels"] = test_set.groups
+    if return_parameters:
+        result["parameters"] = {**hyperparameters, **pure_parameters}
     return result
