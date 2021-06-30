58ab89c7df7c571bba2c95c5d87f02ee095b5592
Arne Küderle <arne.kuederle@fau.de>
2021-06-30 17:29:53 +0200

    better version update command

--- dodo.py
+++ dodo.py
@@ -1,6 +1,7 @@
 import platform
 import re
 import shutil
+import subprocess
 from pathlib import Path
 
 DOIT_CONFIG = {
@@ -64,14 +65,14 @@ def task_register_ipykernel():
 def update_version_strings(file_path, new_version):
     # taken from:
     # https://stackoverflow.com/questions/57108712/replace-updated-version-strings-in-files-via-python
-    version_regex = re.compile(r"(^_*?version_*?\s*=\s*['\"])(\d+\.\d+\.\d+)", re.M)
+    version_regex = re.compile(r"(^_*?version_*?\s*=\s*\")(\d+\.\d+\.\d+-?\S*)\"", re.M)
     with open(file_path, "r+") as f:
         content = f.read()
         f.seek(0)
         f.write(
             re.sub(
                 version_regex,
-                lambda match: "{}{}".format(match.group(1), new_version),
+                lambda match: "{}{}\"".format(match.group(1), new_version),
                 content,
             )
         )
@@ -79,8 +80,14 @@ def update_version_strings(file_path, new_version):
 
 
 def update_version(version):
-    update_version_strings(HERE / "gaitmap/__init__.py", version)
-    update_version_strings(HERE / "pyproject.toml", version)
+    subprocess.run(["poetry", "version", version], shell=False, check=True)
+    new_version = (
+        subprocess.run(["poetry", "version"], shell=False, check=True, capture_output=True)
+        .stdout.decode().strip()
+        .split(" ", 1)[1]
+    )
+    print(new_version)
+    update_version_strings(HERE / "gaitmap/__init__.py", new_version)
 
 
 def task_update_version():
