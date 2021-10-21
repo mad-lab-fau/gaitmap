d42c35f724c683c9502eb075c308aa2260789e9e
Nils Roth <nils.roth@fau.de>
2021-10-21 10:48:34 +0200

    fixed linting

--- gaitmap/preprocessing/sensor_alignment/__init__.py
+++ gaitmap/preprocessing/sensor_alignment/__init__.py
@@ -3,3 +3,5 @@
 from gaitmap.preprocessing.sensor_alignment._gravity_alignment import align_dataset_to_gravity
 from gaitmap.preprocessing.sensor_alignment._mulisensor_alignment import align_heading_of_sensors
 from gaitmap.preprocessing.sensor_alignment._pca_alignment import PcaAlignment
+
+__all__ = ["align_dataset_to_gravity", "align_heading_of_sensors", "PcaAlignment"]
