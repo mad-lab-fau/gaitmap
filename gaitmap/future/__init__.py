62bf0fbfa223fa751147ef69c66ec82f1973e54d
Arne Küderle <arne.kuederle@fau.de>
2021-12-13 16:15:49 +0100

    Completly removed the future folder

--- gaitmap/future/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Temporary module for future features."""
-import warnings
-
-warnings.warn(
-    "All pipeline features in gaitmap are depcrecated and moved to `tpcp`. Please use tpcp directly. In "
-    "most cases this just requires a change of import paths.",
-    DeprecationWarning,
-)
