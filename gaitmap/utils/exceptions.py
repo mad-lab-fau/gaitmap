5a3618bf950d6fe56e8206244204aeb6ca207825
Arne Küderle <arne.kuederle@fau.de>
2022-07-03 22:34:11 +0200

    Removed unused exception

--- gaitmap/utils/exceptions.py
+++ gaitmap/utils/exceptions.py
@@ -5,10 +5,6 @@ class ValidationError(Exception):
     """An error indicating that data-object does not comply with the guidelines."""
 
 
-class PotentialUserErrorWarning(UserWarning):
-    """A warning indicating that the user might not use certain gaitmap features correctly."""
-
-
 class GaitmapMadImportError(ImportError):
     """An error indicating that the algorithm is implemented in gaitmap-mad and not gaitmap."""
 
