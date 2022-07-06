cb22ef6dc78818346ddd56866df004e6d0700466
Hamid Moradi <hamidmoradi@ox31ykoc.vpn.rrze.uni-erlangen.de>
2022-07-06 15:39:44 +0200

    test files for filtered event detection were created. Filtered event detection notes updated. init file corrected

--- gaitmap_mad/gaitmap_mad/event_detection/_filtered_rampp_event_detection.py
+++ gaitmap_mad/gaitmap_mad/event_detection/_filtered_rampp_event_detection.py
@@ -1,22 +1,19 @@
 """The event detection algorithm by Rampp et al. 2014."""
+import dataclasses
 from typing import Dict, Optional, Tuple, Union
 
 from joblib import Memory
 
 from gaitmap.event_detection._event_detection_mixin import FilterParameter
-from gaitmap_mad.event_detection._rampp_event_detection import RamppEventDetection
+from gaitmap.event_detection._rampp_event_detection import RamppEventDetection
 
 
 class FilteredRamppEventDetection(RamppEventDetection):
-    """This addition of the rampp event detection uses a 15 Hz low pass filter on the gyr-ml in ic and tc detection.
+    """This addition uses a lowpass filter only in ic calculation.
 
-    However, the min_velocity calculation remians as standard rampp algorithm.
-    It is suggested to be used on data containg high frequency noise or artifacts which affects minimum detection in
-    rampp algorithm
-
-    RamppEventDetection uses signal processing approaches to find temporal gait events by searching for characteristic
-    features in the foot-mounted sensor signals as described in Rampp et al. (2014) [1]_.
-    For more details refer to the `Notes` section.
+    It is suggested to be used on data containg high frequency noise or artifacts around the ic part of the strides.
+    The tc and min velocity are calculated from the original signal.
+    For more information on Rampp event detection please visit the original rampp event detection.
 
     Parameters
     ----------
@@ -29,8 +26,9 @@ class FilteredRamppEventDetection(RamppEventDetection):
         (1/`sampling_rate_hz`).
     min_vel_search_win_size_ms
         The size of the sliding window for finding the minimum gyroscope energy in ms.
-    cutoff_frequency
-        The selected cut off frequency of the low pass filter.
+    ic_lowpass_filter_parameter
+        A tuple including the information required for a low pass filter design in the format of
+        (order, cutoff frequency)
     memory
         An optional `joblib.Memory` object that can be provided to cache the detection of all events.
     enforce_consistency
@@ -66,20 +64,6 @@ class FilteredRamppEventDetection(RamppEventDetection):
         between subsequent strides. That means for subsequent strides the end sample of one stride should be the
         start sample of the next stride.
 
-    Examples
-    --------
-    Get gait events from single sensor signal
-
-    >>> event_detection = RamppEventDetection()
-    >>> event_detection.detect(data=data, stride_list=stride_list, sampling_rate_hz=204.8)
-    >>> event_detection.min_vel_event_list_
-           start     end      ic      tc  min_vel  pre_ic
-    s_id
-    0      519.0   710.0   651.0   584.0    519.0   498.0
-    1      710.0   935.0   839.0   802.0    710.0   651.0
-    2      935.0  1183.0  1089.0  1023.0    935.0   839.0
-    ...
-
     Notes
     -----
     Rampp et al. implemented the detection of three gait events from foot-mounted sensor data:
@@ -109,43 +93,6 @@ class FilteredRamppEventDetection(RamppEventDetection):
         The default window size is set to 100 ms with 50 % overlap.
         The window size can be adjusted via the `min_vel_search_win_size_ms` parameter.
         Also refer to the :ref:`image below <fe>`.
-
-    The :func:`~gaitmap.event_detection.RamppEventDetection.detect` method provides a stride list `min_vel_event_list`
-    with the gait events mentioned above and additionally `start` and `end` of each stride, which are aligned to the
-    `min_vel` samples.
-    The start sample of each stride corresponds to the min_vel sample of that stride and the end sample corresponds to
-    the min_vel sample of the subsequent stride.
-    Furthermore, the `min_vel_event_list` list provides the `pre_ic` which is the ic event of the previous stride in
-    the stride list.
-
-    The :class:`~gaitmap.event_detection.RamppEventDetection` includes a consistency check that is enabled by default.
-    The gait events within one stride provided by the `stride_list` must occur in the expected order.
-    Any stride where the gait events are detected in a different order or are not detected at all is dropped!
-    For more infos on this see :func:`~gaitmap.utils.stride_list_conversion.enforce_stride_list_consistency`.
-    If you wish to disable this consistency check, set `enforce_consistency` to False.
-    In this case, the attribute `min_vel_event_list_` will not be set, but you can use `segmented_event_list_` to get
-    all detected events for the exact stride list that was used as input.
-    Note, that this list might contain NaN for some events.
-
-    Furthermore, during the conversion from the segmented stride list to the "min_vel" stride list, breaks in
-    continuous gait sequences ( with continuous subsequent strides according to the `stride_list`) are detected and the
-    first (segmented) stride of each sequence is dropped.
-    This is required due to the shift of stride borders between the `stride_list` and the `min_vel_event_list`.
-    Thus, the first segmented stride of a continuous sequence only provides a pre_ic and a min_vel sample for
-    the first stride in the `min_vel_event_list`.
-    Therefore, the `min_vel_event_list` list has one stride less per gait sequence than the `segmented_stride_list`.
-
-    Further information regarding the coordinate system can be found :ref:`here<coordinate_systems>` and regarding the
-    different types of strides can be found :ref:`here<stride_list_guide>`.
-
-    The image below gives an overview about the events and where they occur in the signal.
-
-    .. _fe:
-    .. figure:: /images/event_detection.svg
-
-    .. [1] Rampp, A., Barth, J., Schülein, S., Gaßmann, K. G., Klucken, J., & Eskofier, B. M. (2014). Inertial
-       sensor-based stride parameter calculation from gait sequences in geriatric patients. IEEE transactions on
-       biomedical engineering, 62(4), 1089-1097.. https://doi.org/10.1109/TBME.2014.2368211
 
     """
 
