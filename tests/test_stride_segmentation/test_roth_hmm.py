f19ac148b7000e4d0e4b28ced6a962504573e6cd
Arne Küderle <arne.kuederle@fau.de>
2022-11-14 11:33:56 +0100

    Added test for feature transform

--- tests/test_stride_segmentation/test_roth_hmm.py
+++ tests/test_stride_segmentation/test_roth_hmm.py
@@ -3,7 +3,12 @@ import pandas as pd
 import pytest
 
 from gaitmap.utils.coordinate_conversion import convert_left_foot_to_fbf
-from gaitmap_mad.stride_segmentation.hmm import PreTrainedRothSegmentationModel, RothHMM, SegmentationHMM
+from gaitmap_mad.stride_segmentation.hmm import (
+    PreTrainedRothSegmentationModel,
+    RothHMM,
+    SegmentationHMM,
+    RothHMMFeatureTransformer,
+)
 from tests.mixins.test_algorithm_mixin import TestAlgorithmMixin
 
 
@@ -29,3 +34,21 @@ class TestMetaFunctionalityRothHmm(TestAlgorithmMixin):
         hmm = RothHMM()
         hmm.segment(convert_left_foot_to_fbf(healthy_example_imu_data["left_sensor"]), sampling_rate_hz=100)
         return hmm
+
+
+class TestMetaFunctionalityRothHMMFeatureTransformer(TestAlgorithmMixin):
+    __test__ = True
+
+    algorithm_class = RothHMMFeatureTransformer
+
+    @pytest.fixture()
+    def after_action_instance(
+        self, healthy_example_imu_data, healthy_example_stride_borders
+    ) -> RothHMMFeatureTransformer:
+        transform = RothHMMFeatureTransformer()
+        transform.transform(
+            convert_left_foot_to_fbf(healthy_example_imu_data["left_sensor"]),
+            roi_list=healthy_example_stride_borders["left_sensor"],
+            sampling_rate_hz=100,
+        )
+        return transform
