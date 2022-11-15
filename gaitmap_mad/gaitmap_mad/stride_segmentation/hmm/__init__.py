cd2a207bdccd48df1edf23470ec7166d2047d2b7
Arne Küderle <arne.kuederle@fau.de>
2022-11-15 11:07:07 +0100

    Added Warning about experimental nature of HMMs

--- gaitmap_mad/gaitmap_mad/stride_segmentation/hmm/__init__.py
+++ gaitmap_mad/gaitmap_mad/stride_segmentation/hmm/__init__.py
@@ -1,4 +1,6 @@
 """Roth et al. HMM based stride segmentation model."""
+import warnings
+
 from gaitmap_mad.stride_segmentation.hmm._hmm_feature_transform import (
     BaseHmmFeatureTransformer,
     RothHmmFeatureTransformer,
@@ -9,6 +11,14 @@ from gaitmap_mad.stride_segmentation.hmm._hmm_stride_segmentation import (
 )
 from gaitmap_mad.stride_segmentation.hmm._segmentation_model import BaseSegmentationHmm, RothSegmentationHmm
 from gaitmap_mad.stride_segmentation.hmm._simple_model import SimpleHmm
+
+warnings.warn(
+    "The hmm support in gaitmap is still quite experimental and you might run into some rough edges."
+    "If you encounter any issues, please report them on github. "
+    "Also expect the API to change in the future."
+    "Monitor the changelog before upgrading to newer versions when using HMMs.",
+    UserWarning,
+)
 
 __all__ = [
     "RothHmmFeatureTransformer",
