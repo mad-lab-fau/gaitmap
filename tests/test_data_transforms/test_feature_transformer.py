b084fa08de1e0b4b6e860028387123b7e562089d
Arne Küderle <arne.kuederle@fau.de>
2022-11-16 10:36:22 +0100

    Added test for most sliding window transformers

--- tests/test_data_transforms/test_feature_transformer.py
+++ tests/test_data_transforms/test_feature_transformer.py
@@ -1,5 +1,10 @@
+from typing import Type, Any, Dict, Callable
+
+import numpy as np
+import pandas as pd
 import pytest
-from pandas._testing import assert_frame_equal
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+from pandas._testing import assert_frame_equal, assert_series_equal
 
 from gaitmap.data_transform import (
     Resample,
@@ -9,14 +14,14 @@ from gaitmap.data_transform import (
     SlidingWindowGradient,
     BaseTransformer,
 )
+from gaitmap.data_transform._feature_transform import _PandasRollingFeatureTransform
 from gaitmap.utils.consts import BF_COLS
 from tests.mixins.test_algorithm_mixin import TestAlgorithmMixin
 
 all_rolling_transformer = [
-    (SlidingWindowMean, {"window_size_s": 1}),
-    (SlidingWindowStd, {"window_size_s": 1}),
-    (SlidingWindowVar, {"window_size_s": 1}),
-    (SlidingWindowGradient, {"window_size_s": 1}),
+    (SlidingWindowMean, {"window_size_s": 1}, np.mean),
+    (SlidingWindowStd, {"window_size_s": 1}, lambda _df, axis: _df.std(axis=axis)),
+    (SlidingWindowVar, {"window_size_s": 1}, lambda _df, axis: _df.var(axis=axis)),
 ]
 
 
@@ -39,9 +44,11 @@ class TestMetaFunctionalityResample(TestAlgorithmMixin):
 class TestMetaFunctionalityRollingTransforms(TestAlgorithmMixin):
     __test__ = True
 
-    @pytest.fixture(params=all_rolling_transformer, autouse=True)
+    @pytest.fixture(
+        params=[*all_rolling_transformer, (SlidingWindowGradient, {"window_size_s": 1}, None)], autouse=True
+    )
     def set_algo_class(self, request):
-        self.algorithm_class, self.algo_params = request.param
+        self.algorithm_class, self.algo_params, _ = request.param
 
     @pytest.fixture()
     def after_action_instance(self, healthy_example_imu_data, healthy_example_stride_borders) -> BaseTransformer:
@@ -52,16 +59,17 @@ class TestMetaFunctionalityRollingTransforms(TestAlgorithmMixin):
         return after_instance
 
 
-class TestResample:
-    @pytest.fixture(params=["df", "series"])
-    def df_or_series_imu_data(self, request, healthy_example_imu_data):
-        """A ficture that returns either a df or a series of imu data."""
-        data_left = healthy_example_imu_data["left_sensor"].iloc[:1000]
-        data_left.columns = BF_COLS
-        if request.param == "df":
-            return data_left
-        return data_left.iloc[:, 0]
+@pytest.fixture(params=["df", "series"])
+def df_or_series_imu_data(request, healthy_example_imu_data):
+    """A ficture that returns either a df or a series of imu data."""
+    data_left = healthy_example_imu_data["left_sensor"].iloc[:1000]
+    data_left.columns = BF_COLS
+    if request.param == "df":
+        return data_left
+    return data_left.iloc[:, 0]
 
+
+class TestResample:
     def test_resample(self, df_or_series_imu_data):
         instance = Resample(target_sampling_rate_hz=10)
         after_instance = instance.transform(df_or_series_imu_data, sampling_rate_hz=100)
@@ -112,3 +120,60 @@ class TestResample:
             instance.transform(sampling_rate_hz=10)
 
         assert "target_sampling_rate_hz" in str(e.value)
+
+
+class TestSlidingWindowTransformers:
+    algorithm_class: Type[_PandasRollingFeatureTransform]
+    algo_params: Dict[str, Any]
+    equivalent_method: Callable
+
+    @pytest.fixture(params=all_rolling_transformer, autouse=True)
+    def set_algo_class(self, request):
+        self.algorithm_class, self.algo_params, self.equivalent_method = request.param
+
+    @pytest.mark.parametrize(
+        "window_size_s,effective_win_size", [(1, 101), (0.5, 51), (0.1, 11), (0.23, 23), (0.111, 11)]
+    )
+    def test_effective_window_size_samples(self, healthy_example_imu_data, window_size_s, effective_win_size):
+        data_left = healthy_example_imu_data["left_sensor"].iloc[:100]
+        data_left.columns = BF_COLS
+        instance = self.algorithm_class(window_size_s=window_size_s)
+        after_instance = instance.transform(data_left, sampling_rate_hz=100)
+        assert after_instance.effective_window_size_samples_ == effective_win_size
+
+    def test_output(self, df_or_series_imu_data):
+        """Test the output shape and the values for the first couple of windows."""
+        win_size_s = 0.1
+        instance = self.algorithm_class(window_size_s=win_size_s)
+        after_instance = instance.transform(df_or_series_imu_data, sampling_rate_hz=100)
+
+        # Test the shape
+        assert after_instance.transformed_data_.shape == df_or_series_imu_data.shape
+        # Test output column names
+        if isinstance(df_or_series_imu_data, pd.DataFrame):
+            assert after_instance.transformed_data_.columns.tolist() == [
+                f"{self.algorithm_class._prefix}__{col}" for col in BF_COLS
+            ]
+        else:
+            assert after_instance.transformed_data_.name == f"{self.algorithm_class._prefix}__{BF_COLS[0]}"
+        # Assert value of first window
+        assert_array_almost_equal(
+            after_instance.transformed_data_.iloc[:1].to_numpy().flatten(),
+            self.equivalent_method(pd.DataFrame(df_or_series_imu_data).iloc[:6], axis=0).to_numpy().flatten(),
+        )
+
+        # Assert the first full window
+        assert_array_almost_equal(
+            after_instance.transformed_data_.iloc[5:6].to_numpy().flatten(),
+            self.equivalent_method(pd.DataFrame(df_or_series_imu_data).iloc[:11], axis=0).to_numpy().flatten(),
+        )
+
+        # Assert the last window
+        assert_array_almost_equal(
+            after_instance.transformed_data_.iloc[-1:].to_numpy().flatten(),
+            self.equivalent_method(pd.DataFrame(df_or_series_imu_data).iloc[-6:], axis=0).to_numpy().flatten(),
+        )
+
+        # Test that data and sampling rate are set as attributes
+        assert after_instance.data is df_or_series_imu_data
+        assert after_instance.sampling_rate_hz == 100
