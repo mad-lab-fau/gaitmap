dd2889f6021a87e7d78e471c420d1517e3175176
Arne Küderle <arne.kuederle@fau.de>
2022-11-17 22:02:03 +0100

    Disabled unreleibale test

--- tests/test_examples/test_all_examples.py
+++ tests/test_examples/test_all_examples.py
@@ -255,6 +255,8 @@ def test_roth_hmm_stride_segmentation(snapshot):
 def test_segmentation_hmm_training(snapshot):
     from examples.stride_segmentation.segmentation_hmm_training import hmm, segmentation_model
 
-    snapshot.assert_match(segmentation_model.model.to_json())
+    # XXX: For some sad reason the training does not seem to be deterministic accross different machines.
+    #      Therefore, we will just check that the model will still find the same strides for now.
+    # snapshot.assert_match(segmentation_model.model.to_json())
     snapshot.assert_match(hmm.stride_list_["left_sensor"], "left_sensor")
     snapshot.assert_match(hmm.stride_list_["right_sensor"], "right_sensor")
