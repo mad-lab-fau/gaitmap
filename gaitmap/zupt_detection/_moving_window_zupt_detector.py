a583fedc0e0a6bfeff6790b635b078533da700c0
Arne Küderle <arne.kuederle@fau.de>
2023-01-09 17:57:52 +0100

    Added Ared Zupt detector and added some more tests

--- gaitmap/zupt_detection/_moving_window_zupt_detector.py
+++ gaitmap/zupt_detection/_moving_window_zupt_detector.py
@@ -7,7 +7,6 @@ from typing_extensions import Literal, Self
 
 from gaitmap.base import BaseZuptDetector
 from gaitmap.utils.array_handling import bool_array_to_start_end_array
-from gaitmap.utils.consts import SF_ACC, SF_GYR
 from gaitmap.utils.datatype_helper import SingleSensorData, is_single_sensor_data
 from gaitmap.utils.exceptions import ValidationError
 from gaitmap.utils.static_moment_detection import METRIC_FUNCTION_NAMES, find_static_samples, find_static_samples_shoe
@@ -15,7 +14,12 @@ from gaitmap.utils.static_moment_detection import METRIC_FUNCTION_NAMES, find_st
 SENSOR_NAMES = Literal["acc", "gyr"]  # noqa: invalid-name
 
 
-def _validate_window(window_length_s: float, window_overlap: float, sampling_rate_hz: float) -> Tuple[int, int]:
+def _validate_window(
+    window_length_s: float,
+    window_overlap: Optional[float],
+    window_overlap_samples: Optional[int],
+    sampling_rate_hz: float,
+) -> Tuple[int, Optional[int]]:
     """Validate window_length and overlap."""
     window_length = round(sampling_rate_hz * window_length_s)
     if window_length < 3:
@@ -24,16 +28,37 @@ def _validate_window(window_length_s: float, window_overlap: float, sampling_rat
             f"`window_length_s`={window_length_s}). "
             "Specify a larger window length."
         )
-    if not 0 <= window_overlap < 1:
-        raise ValidationError("`window_overlap` must be `0 <= window_overlap < 1`")
-
-    window_overlap_samples = round(window_length * window_overlap)
-    if window_overlap_samples == window_length:
+    # Exactly one of window_overlap and window_overlap_samples must be specified
+    if (window_overlap is not None and window_overlap_samples is not None) or (
+        window_overlap is None and window_overlap_samples is None
+    ):
         raise ValidationError(
-            "The effective window overlap after rounding is 1, because the window is to short. "
-            "Either choose a smaller overlap or a larger window."
+            "Specify either `window_overlap` or `window_overlap_samples`, not both. "
+            "Set the one you don't want to specify to `None`."
         )
-    return window_length, window_overlap_samples
+
+    if window_overlap is not None:
+        if not 0 <= window_overlap < 1:
+            raise ValidationError("`window_overlap` must be `0 <= window_overlap < 1`")
+
+        window_overlap_samples_out = round(window_length * window_overlap)
+        if window_overlap_samples_out == window_length:
+            raise ValidationError(
+                "The effective window overlap after rounding is 1, because the window is to short. "
+                "Either choose a smaller overlap or a larger window."
+            )
+    else:
+        if not isinstance(window_overlap_samples, int):
+            raise ValidationError("`window_overlap_samples` must be an integer")
+        if window_overlap_samples < 0:
+            window_overlap_samples = window_length + window_overlap_samples
+        if not 0 <= window_overlap_samples < window_length:
+            raise ValidationError(
+                "`window_overlap_samples` must be `0 <= window_overlap_samples < window_length` or"
+                "`-window_length < window_overlap_samples < 0`"
+            )
+        window_overlap_samples_out = window_overlap_samples
+    return window_length, window_overlap_samples_out
 
 
 class _PerSampleDetectorMixin:
@@ -60,9 +85,11 @@ class NormZuptDetector(BaseZuptDetector, _PerSampleDetectorMixin):
 
     At the moment only single sensor data is supported.
 
-    ... note::
+    .. note::
         Using the gyro signal with the metric "squared_mean" and a window overlap of len(window) - 1 samples, this ZUPT
         detector is equivalent to the ARED Zupt detector presented in [1]_.
+        We also provide the :class:`~gaitmap.zupt_detection.AredZuptDetector` class for convenience that has these
+        values as default.
 
     .. warning::
         Due to edge cases at the end of the input data where window size and overlap might not fit your data, the last
@@ -80,7 +107,19 @@ class NormZuptDetector(BaseZuptDetector, _PerSampleDetectorMixin):
         The overlap between two neighboring windows as a fraction of the window length.
         Must be `0 <= window_overlap < 1`.
         Note that the window length is first converted into samples, before the effective overlap is calculated.
-        The overlap in samples is calculated as `overlap_samples = round(window_length_samples * window_overlap)`
+        The overlap in samples is calculated as `overlap_samples = round(window_length_samples * window_overlap)`.
+
+        When `window_overlap` is specified, `window_overlap_samples` must be `None`.
+    window_overlap_samples : int
+        The overlap between two neighboring windows in samples.
+        This can be used as alternative to `window_overlap`, when you want to set the overlap independent of the
+        sampling rate.
+        The values can be negative.
+        In this case, it will be interpreted as len(window_length_samples) - window_overlap_samples.
+        This can be helpful if you want to set the overlap to the maximum possible value.
+        Then set `window_overlap_samples` to -1.
+
+        When `window_overlap_samples` is specified, `window_overlap` must be `None`.
     inactive_signal_threshold : float
        Threshold to decide whether a window should be considered as active or inactive.
        Window will be tested on `metric(norm_window) <= threshold`.
@@ -115,7 +154,7 @@ class NormZuptDetector(BaseZuptDetector, _PerSampleDetectorMixin):
         If the value is `True` for a sample, it is part of a static region.
     window_length_samples_
         The internally calculated window length in samples.
-        This might be helpful for debugging
+        This might be helpful for debugging.
     window_overlap_samples_
         The internally calculated window overlap in samples.
         This might be helpful for debugging.
@@ -134,11 +173,9 @@ class NormZuptDetector(BaseZuptDetector, _PerSampleDetectorMixin):
     sensor: SENSOR_NAMES
     window_length_s: float
     window_overlap: Optional[float]
+    window_overlap_samples: Optional[int]
     metric: METRIC_FUNCTION_NAMES
     inactive_signal_threshold: float
-
-    data: SingleSensorData
-    sampling_rate_hz: float
 
     window_length_samples_: int
     window_overlap_samples_: int
@@ -148,17 +185,19 @@ class NormZuptDetector(BaseZuptDetector, _PerSampleDetectorMixin):
         *,
         sensor: SENSOR_NAMES = "gyr",
         window_length_s: float = 0.15,
-        window_overlap: float = 0.5,
+        window_overlap: Optional[float] = 0.5,
+        window_overlap_samples: Optional[int] = None,
         metric: METRIC_FUNCTION_NAMES = "mean",
         inactive_signal_threshold: float = 15,
     ):
         self.sensor = sensor
         self.window_length_s = window_length_s
         self.window_overlap = window_overlap
+        self.window_overlap_samples = window_overlap_samples
         self.metric = metric
         self.inactive_signal_threshold = inactive_signal_threshold
 
-    def detect(self: Self, data: SingleSensorData, sampling_rate_hz: float, **kwargs) -> Self:
+    def detect(self, data: SingleSensorData, sampling_rate_hz: float, **kwargs) -> Self:
         """Detect all ZUPT regions in the data.
 
         Parameters
@@ -182,7 +221,7 @@ class NormZuptDetector(BaseZuptDetector, _PerSampleDetectorMixin):
         )
 
         self.window_length_samples_, self.window_overlap_samples_ = _validate_window(
-            self.window_length_s, self.window_overlap, self.sampling_rate_hz
+            self.window_length_s, self.window_overlap, self.window_overlap_samples, self.sampling_rate_hz
         )
 
         self.per_sample_zupts_ = find_static_samples(
@@ -194,6 +233,109 @@ class NormZuptDetector(BaseZuptDetector, _PerSampleDetectorMixin):
         )
 
         return self
+
+
+class AredZuptDetector(NormZuptDetector):
+    """The angular rate energy detector (ARED) for ZUPT detection.
+
+    This detector is a special case of the :class:`~gaitmap.zupt_detection.NormZuptDetector` with the metric set to
+    `squared_mean` and a window overlap of `len(window) - 1`.
+    Besides the defaults, this class is identical to :class:`~gaitmap.zupt_detection.NormZuptDetector`.
+
+    This ZUPT detector is based on [1]_ and [2]_.
+
+    Parameters
+    ----------
+    sensor
+        The sensor (either `acc` or `gyr`) to apply the detection to.
+        The detector will calculate the norm for the respective sensor.
+    window_length_s : int
+        Length of desired window in seconds.
+        The real window length is calculated as `window_length_samples = round(sampling_rate_hz * window_length_s)`
+    window_overlap : float
+        The overlap between two neighboring windows as a fraction of the window length.
+        Must be `0 <= window_overlap < 1`.
+        Note that the window length is first converted into samples, before the effective overlap is calculated.
+        The overlap in samples is calculated as `overlap_samples = round(window_length_samples * window_overlap)`.
+
+        When `window_overlap` is specified, `window_overlap_samples` must be `None`.
+    window_overlap_samples : int
+        The overlap between two neighboring windows in samples.
+        This can be used as alternative to `window_overlap`, when you want to set the overlap independent of the
+        sampling rate.
+        The values can be negative.
+        In this case, it will be interpreted as len(window_length_samples) - window_overlap_samples.
+        This can be helpful if you want to set the overlap to the maximum possible value.
+        Then set `window_overlap_samples` to -1.
+
+        When `window_overlap_samples` is specified, `window_overlap` must be `None`.
+    inactive_signal_threshold : float
+       Threshold to decide whether a window should be considered as active or inactive.
+       Window will be tested on `metric(norm_window) <= threshold`.
+       The unit of this value depends on the selected sensor and metric.
+    metric : str
+        Metric which will be calculated per window on the norm of the signal, one of the following strings
+
+        'mean' (default)
+            Calculates mean value per window
+        'squared_mean'
+            The same as mean, but the norm of the signal is squared before calculating the mean.
+        'maximum'
+            Calculates maximum value per window
+        'median'
+            Calculates median value per window
+        'variance'
+            Calculates variance value per window
+
+    Other Parameters
+    ----------------
+    data
+        The data passed to the detect method
+    sampling_rate_hz
+        The sampling rate of this data
+
+    Attributes
+    ----------
+    zupts_
+        A dataframe with the columns `start` and `end` specifying the start and end of all static regions in samples
+    per_sample_zupts_
+        A bool array with length `len(data)`.
+        If the value is `True` for a sample, it is part of a static region.
+    window_length_samples_
+        The internally calculated window length in samples.
+        This might be helpful for debugging.
+    window_overlap_samples_
+        The internally calculated window overlap in samples.
+        This might be helpful for debugging.
+
+    References
+    ----------
+    .. [1] I. Skog, J.-O. Nilsson, P. Händel, and J. Rantakokko, “Zero-velocity detection—An algorithm evaluation,”
+       IEEE Trans. Biomed. Eng., vol. 57, no. 11, pp. 2657–2666, Nov. 2010.
+    .. [2] Wagstaff, Peretroukhin, and Kelly, “Robust Data-Driven Zero-Velocity Detection for Foot-Mounted Inertial
+       Navigation.”
+
+
+    """
+
+    def __init__(
+        self,
+        *,
+        sensor: SENSOR_NAMES = "gyr",
+        window_length_s: float = 0.15,
+        window_overlap: Optional[float] = None,
+        window_overlap_samples: Optional[int] = -1,
+        metric: METRIC_FUNCTION_NAMES = "squared_mean",
+        inactive_signal_threshold: float = 225,
+    ):
+        super().__init__(
+            sensor=sensor,
+            window_length_s=window_length_s,
+            window_overlap=window_overlap,
+            window_overlap_samples=window_overlap_samples,
+            metric=metric,
+            inactive_signal_threshold=inactive_signal_threshold,
+        )
 
 
 class ShoeZuptDetector(BaseZuptDetector, _PerSampleDetectorMixin):
@@ -225,6 +367,23 @@ class ShoeZuptDetector(BaseZuptDetector, _PerSampleDetectorMixin):
         The overlap between two neighboring windows as a fraction of the window length.
         Must be `0 <= window_overlap < 1`.
         Note that the window length is first converted into samples, before the effective overlap is calculated.
+        The overlap in samples is calculated as `overlap_samples = round(window_length_samples * window_overlap)`.
+
+        When `window_overlap` is specified, `window_overlap_samples` must be `None`.
+    window_overlap_samples : int
+        The overlap between two neighboring windows in samples.
+        This can be used as alternative to `window_overlap`, when you want to set the overlap independent of the
+        sampling rate.
+        The values can be negative.
+        In this case, it will be interpreted as len(window_length_samples) - window_overlap_samples.
+        This can be helpful if you want to set the overlap to the maximum possible value.
+        Then set `window_overlap_samples` to -1.
+
+        When `window_overlap_samples` is specified, `window_overlap` must be `None`.
+    window_overlap : float
+        The overlap between two neighboring windows as a fraction of the window length.
+        Must be `0 <= window_overlap < 1`.
+        Note that the window length is first converted into samples, before the effective overlap is calculated.
         The overlap in samples is calculated as `overlap_samples = round(window_length_samples * window_overlap)`
 
     Other Parameters
@@ -248,11 +407,18 @@ class ShoeZuptDetector(BaseZuptDetector, _PerSampleDetectorMixin):
         The internally calculated window overlap in samples.
         This might be helpful for debugging.
 
+    References
+    ----------
+    .. [1] I. Skog, J.-O. Nilsson, P. Händel, and J. Rantakokko, “Zero-velocity detection—An algorithm evaluation,”
+       IEEE Trans. Biomed. Eng., vol. 57, no. 11, pp. 2657–2666, Nov. 2010.
+    .. [2] Wagstaff, Peretroukhin, and Kelly, “Robust Data-Driven Zero-Velocity Detection for Foot-Mounted Inertial
+       Navigation.”
 
     """
 
     window_length_s: float
     window_overlap: Optional[float]
+    window_overlap_samples: Optional[int]
     inactive_signal_threshold: float
     acc_noise_variance: float
     gyr_noise_variance: float
@@ -269,16 +435,18 @@ class ShoeZuptDetector(BaseZuptDetector, _PerSampleDetectorMixin):
         acc_noise_variance: float = 10e-8,
         gyr_noise_variance: float = 10e-9,
         window_length_s: float = 0.15,
-        window_overlap: float = 0.5,
+        window_overlap: Optional[float] = 0.5,
+        window_overlap_samples: Optional[int] = None,
         inactive_signal_threshold: float = 15,
     ):
         self.acc_noise_variance = acc_noise_variance
         self.gyr_noise_variance = gyr_noise_variance
         self.window_length_s = window_length_s
         self.window_overlap = window_overlap
+        self.window_overlap_samples = window_overlap_samples
         self.inactive_signal_threshold = inactive_signal_threshold
 
-    def detect(self: Self, data: SingleSensorData, sampling_rate_hz: float, **kwargs) -> Self:
+    def detect(self, data: SingleSensorData, sampling_rate_hz: float, **kwargs) -> Self:
         """Detect all ZUPT regions in the data.
 
         Parameters
@@ -300,7 +468,7 @@ class ShoeZuptDetector(BaseZuptDetector, _PerSampleDetectorMixin):
         is_single_sensor_data(self.data, frame="any", raise_exception=True)
 
         self.window_length_samples_, self.window_overlap_samples_ = _validate_window(
-            self.window_length_s, self.window_overlap, self.sampling_rate_hz
+            self.window_length_s, self.window_overlap, self.window_overlap_samples, self.sampling_rate_hz
         )
 
         self.per_sample_zupts_ = find_static_samples_shoe(
