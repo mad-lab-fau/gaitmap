76afb3d05e860a95b00554c3ae49adf1e6a1b0ee
Arne Küderle <arne.kuederle@fau.de>
2023-02-07 17:29:47 +0100

    Added first implementation of a PerStrideZupt detector

--- gaitmap/zupt_detection/__init__.py
+++ gaitmap/zupt_detection/__init__.py
@@ -1,4 +1,5 @@
 """A set of methods to detect static regions/zero-velocity regions (ZUPTS) in a signal."""
+from gaitmap.zupt_detection._stride_event_zupt_detector import StrideEventZuptDetector
 from gaitmap.zupt_detection._moving_window_zupt_detector import AredZuptDetector, NormZuptDetector, ShoeZuptDetector
 
-__all__ = ["NormZuptDetector", "ShoeZuptDetector", "AredZuptDetector"]
+__all__ = ["NormZuptDetector", "ShoeZuptDetector", "AredZuptDetector", "StrideEventZuptDetector"]
