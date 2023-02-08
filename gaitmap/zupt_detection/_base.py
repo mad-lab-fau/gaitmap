64292d39c1bb6384f7811aa3212f87be337ac9fa
Arne Küderle <arne.kuederle@fau.de>
2023-02-08 13:15:49 +0100

    Made Zupt Mixins public

---
+++ gaitmap/zupt_detection/_base.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+
+from gaitmap.utils.array_handling import bool_array_to_start_end_array
+from gaitmap.utils.datatype_helper import SingleSensorData
+
+
+class PerSampleZuptDetectorMixin:
+    """A mixin for ZUPT detectors that internally detect ZUPTs per samples.
+
+    It automatically provides the `zupts_` property that converts the `per_sample_zupts_` to a dataframe of start/end
+    values.
+    """
+
+    per_sample_zupts_: np.ndarray
+
+    @property
+    def zupts_(self) -> pd.DataFrame:
+        """Get the start and end values of all zupts."""
+        start_ends = bool_array_to_start_end_array(self.per_sample_zupts_)
+        if len(start_ends) > 0:
+            return pd.DataFrame(start_ends, columns=["start", "end"])
+        return pd.DataFrame(columns=["start", "end"])
+
+
+class RegionZuptDetectorMixin:
+    """A mixin for ZUPT detectors that internally detect ZUPTs per regions.
+
+    If a detector detects ZUPTs per region (i.e. start and end values), it can use this mixin to automatically provide
+    the `per_sample_zupts_` property.
+
+    Note, that for this to work the `data` attribute must be set as part of the detect method, as we need the length of
+    the data to create the `per_sample_zupts_` array.
+    """
+
+    data: SingleSensorData
+    zupts_: pd.DataFrame
+
+    @property
+    def per_sample_zupts_(self) -> np.ndarray:
+        """Get a bool array of length data with all Zupts as True."""
+        zupts = np.zeros(self.data.shape[0], dtype=bool)
+        for _, row in self.zupts_.iterrows():
+            zupts[row["start"] : row["end"]] = True
+        return zupts
