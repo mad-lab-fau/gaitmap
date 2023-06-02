dc988aba50ca2d93322c126b328ce112dd5d6402
Arne Küderle <arne.kuederle@fau.de>
2023-06-02 15:57:16 +0200

    Warning only occurs once in multiprocessing context

--- gaitmap_mad/gaitmap_mad/stride_segmentation/hmm/__init__.py
+++ gaitmap_mad/gaitmap_mad/stride_segmentation/hmm/__init__.py
@@ -1,4 +1,5 @@
 """Roth et al. HMM based stride segmentation model."""
+import multiprocessing
 import warnings
 
 from gaitmap_mad.stride_segmentation.hmm._hmm_feature_transform import (
@@ -12,13 +13,14 @@ from gaitmap_mad.stride_segmentation.hmm._hmm_stride_segmentation import (
 from gaitmap_mad.stride_segmentation.hmm._segmentation_model import BaseSegmentationHmm, RothSegmentationHmm
 from gaitmap_mad.stride_segmentation.hmm._simple_model import SimpleHmm
 
-warnings.warn(
-    "The hmm support in gaitmap is still quite experimental and you might run into some rough edges."
-    "If you encounter any issues, please report them on github. "
-    "Also expect the API to change in the future."
-    "Monitor the changelog before upgrading to newer versions when using HMMs.",
-    UserWarning,
-)
+if multiprocessing.parent_process() is None:
+    warnings.warn(
+        "The hmm support in gaitmap is still quite experimental and you might run into some rough edges."
+        "If you encounter any issues, please report them on github. "
+        "Also expect the API to change in the future."
+        "Monitor the changelog before upgrading to newer versions when using HMMs.",
+        UserWarning,
+    )
 
 __all__ = [
     "RothHmmFeatureTransformer",
